dcf75f00c0eaa2abc08c327535e320d76746f190
Daniel A. R. Werner <daniel.a.r.werner@gmail.com>
2020-02-07 18:04:49 +0100

    Remove redundant WidgetApiContext

--- packages/rich-text/src/plugins/shared/WidgetApiContext.js
+++ packages/rich-text/src/plugins/shared/WidgetApiContext.js
@@ -1,5 +1,7 @@
 // TODO: Remove this helper and replace usage with PluginApi usage.
+
 import { createContext } from 'react';
-var WidgetApiContext = createContext({});
+
+const WidgetApiContext = createContext({});
+
 export default WidgetApiContext;
-//# sourceMappingURL=WidgetApiContext.js.map
--- packages/rich-text/src/plugins/shared/WidgetApiContext.jsx
+++
@@ -1,7 +0,0 @@
-// TODO: Remove this helper and replace usage with PluginApi usage.
-
-import { createContext } from 'react';
-
-const WidgetApiContext = createContext({});
-
-export default WidgetApiContext;
