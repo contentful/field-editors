0778c4d370eb2e388e87e7a5f31a225d0c62489d
Daniel A. R. Werner <daniel.a.r.werner@gmail.com>
2020-02-13 09:49:57 +0100

    Remove WidgetApiContext

--- packages/rich-text/src/plugins/shared/WidgetApiContext.js
+++
@@ -1,7 +0,0 @@
-// TODO:xxx Remove this helper and replace usage with `richTextAPI.widgetAPI` usage.
-
-import { createContext } from 'react';
-
-const WidgetApiContext = createContext({});
-
-export default WidgetApiContext;
