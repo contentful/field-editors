bb2da6ffd1f484a3d058ce72ce72f5357bbf6353
Daniel A. R. Werner <daniel.a.r.werner@gmail.com>
2020-02-14 10:23:44 +0100

    Remove unused files

--- packages/rich-text/src/RichTextEditorConnected.js
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-export function RichTextEditor() {
-    return React.createElement("div", null, "!!! RichTextEditor !!!");
-}
-RichTextEditor.defaultProps = {
-    isInitiallyDisabled: true
-};
-//# sourceMappingURL=RichTextEditorConnected.js.map
