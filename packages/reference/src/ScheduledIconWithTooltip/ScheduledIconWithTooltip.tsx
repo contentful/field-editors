7779ac67046005208fcbe2aed8f04d5d8d015c22
Alex Suevalov <alex.suevalov@contentful.com>
2020-03-16 16:49:31 +0100

    feat(reference): add schedule icons for entry card component

---
+++ packages/reference/src/ScheduledIconWithTooltip/ScheduledIconWithTooltip.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { SpaceAPI, ScheduledAction } from 'contentful-ui-extensions-sdk';
+import { ScheduleTooltip } from './ScheduleTooltip';
+
+type ScheduledIconWithTooltipProps = {
+  getEntityScheduledActions: SpaceAPI['getEntityScheduledActions'];
+  entityType: 'Entry' | 'Asset';
+  entityId: string;
+  children: React.ReactElement;
+};
+
+export const ScheduledIconWithTooltip = ({
+  entityType,
+  entityId,
+  getEntityScheduledActions,
+  children
+}: ScheduledIconWithTooltipProps) => {
+  const [status, setStatus] = React.useState<
+    | { type: 'loading' }
+    | { type: 'error'; error: Error }
+    | { type: 'loaded'; jobs: ScheduledAction[] }
+  >({ type: 'loading' });
+
+  React.useEffect(() => {
+    getEntityScheduledActions(entityType, entityId)
+      .then(data => {
+        setStatus({ type: 'loaded', jobs: data });
+      })
+      .catch(e => {
+        setStatus({ type: 'error', error: e });
+      });
+  }, []);
+
+  if (status.type === 'loading' || status.type === 'error') {
+    return null;
+  }
+
+  const jobs = status.jobs ? status.jobs : [];
+  const mostRelevantJob = jobs[0];
+
+  return (
+    <ScheduleTooltip job={mostRelevantJob} jobsCount={jobs.length}>
+      {children}
+    </ScheduleTooltip>
+  );
+};
