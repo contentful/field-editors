3aa4ae7a701e98e4c57a6bd2c38df629e1166d7d
John Whiles <john.whiles@contentful.com>
2020-04-23 10:49:14 +0200

    feat: create markdown field editor extension

---
+++ extensions/markdown-extension/src/index.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+import { init, FieldExtensionSDK } from 'contentful-ui-extensions-sdk';
+import '@contentful/forma-36-react-components/dist/styles.css';
+import '@contentful/forma-36-fcss/dist/styles.css';
+import './index.css';
+import '../../../packages/markdown/src/codemirrorImports';
+import { MarkdownEditor } from '../../../packages/markdown/src/index';
+
+interface AppProps {
+  sdk: FieldExtensionSDK;
+}
+
+interface AppState {
+  value?: string;
+}
+
+export class App extends React.Component<AppProps, AppState> {
+  render = () => {
+    return (
+      <MarkdownEditor
+        isInitiallyDisabled={false}
+        parameters={{
+          instance: {
+            canUploadAssets: true
+          }
+        }}
+        sdk={this.props.sdk}
+      />
+    );
+  };
+}
+
+init((sdk: FieldExtensionSDK) => {
+  sdk.window.startAutoResizer();
+  render(<App sdk={sdk as FieldExtensionSDK} />, document.getElementById('root'));
+});
+
+/**
+ * By default, iframe of the extension is fully reloaded on every save of a source file.
+ * If you want to use HMR (hot module reload) instead of full reload, uncomment the following lines
+ */
+// if (module.hot) {
+//   module.hot.accept();
+// }
