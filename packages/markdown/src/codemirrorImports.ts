283ca5619bb3ed3c4423dcf87a043edef0752d6c
John Whiles <john.whiles@contentful.com>
2020-04-23 10:48:51 +0200

    fix: add codemirror.css import to markdown imports file

--- packages/markdown/src/codemirrorImports.ts
+++ packages/markdown/src/codemirrorImports.ts
@@ -2,3 +2,4 @@ import 'codemirror/mode/markdown/markdown';
 import 'codemirror/mode/xml/xml';
 import 'codemirror/addon/edit/continuelist';
 import 'codemirror/addon/mode/overlay';
+import 'codemirror/lib/codemirror.css';
