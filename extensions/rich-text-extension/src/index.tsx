72ed1b70b68f2cc4d1d1620488537e1cedd45136
John Whiles <johnwhiles@outlook.com>
2020-04-23 16:31:35 +0200

    feat: create rich text extension example (#96)
    * fix: add missing dependencies to rich-text-editor
    
    * feat: create rich text field extension example
    
    * fix: use the correct field type in rich text extension
    
    * feat: update forma dependencies in rich text editor
    
    * feat: handle dialog in rich text extension
    
    * refactor: remove unused type

---
+++ extensions/rich-text-extension/src/index.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+import { init, FieldExtensionSDK, locations } from 'contentful-ui-extensions-sdk';
+import '@contentful/forma-36-react-components/dist/styles.css';
+import '@contentful/forma-36-fcss/dist/styles.css';
+import './index.css';
+import { RichTextEditor, renderRichTextDialog } from '../../../packages/rich-text/src/index';
+
+interface AppProps {
+  sdk: FieldExtensionSDK;
+}
+
+export class App extends React.Component<AppProps> {
+  render = () => {
+    const sdk = Object.assign(this.props.sdk, {
+      parameters: {
+        instance: {
+          permissions: {
+            canAccessAssets: true,
+            canCreateAssets: true,
+            canCreateEntryOfContentType: () => true
+          }
+        }
+      }
+    });
+    return <RichTextEditor sdk={sdk} />;
+  };
+}
+
+init((sdk: FieldExtensionSDK) => {
+  sdk.window.startAutoResizer();
+  if (sdk.location.is(locations.LOCATION_DIALOG)) {
+    render(renderRichTextDialog(sdk), document.getElementById('root'));
+  } else {
+    render(<App sdk={sdk} />, document.getElementById('root'));
+  }
+});
+
+/**
+ * By default, iframe of the extension is fully reloaded on every save of a source file.
+ * If you want to use HMR (hot module reload) instead of full reload, uncomment the following lines
+ */
+// if (module.hot) {
+//   module.hot.accept();
+// }
