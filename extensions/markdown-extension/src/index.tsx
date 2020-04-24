e73c953ebf9dc8f1949e953d7fbb77535c4f4d68
John Whiles <johnwhiles@outlook.com>
2020-04-24 16:06:35 +0200

    feat: create markdown field extension (#97)
    * fix: add codemirror.css import to markdown imports file
    
    * feat: create markdown field editor extension
    
    * feat: update contentful dependencies
    
    * feat: handle dialog in markdown extension
    
    * feat(markdown extension): change eslintrc and scripts to match rich text
    
    * fix: remove codemirror import from markdown package
    
    to see if it fixes integration tests

---
+++ extensions/markdown-extension/src/index.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+import {
+  init,
+  locations,
+  FieldExtensionSDK,
+  DialogExtensionSDK
+} from 'contentful-ui-extensions-sdk';
+import '@contentful/forma-36-react-components/dist/styles.css';
+import '@contentful/forma-36-fcss/dist/styles.css';
+import './index.css';
+import '../../../packages/markdown/src/codemirrorImports';
+import 'codemirror/lib/codemirror.css';
+import { MarkdownEditor, renderMarkdownDialog } from '../../../packages/markdown/src/index';
+
+interface AppProps {
+  sdk: FieldExtensionSDK;
+}
+
+export class App extends React.Component<AppProps> {
+  render = () => {
+    return (
+      <MarkdownEditor
+        isInitiallyDisabled={false}
+        parameters={{
+          instance: {
+            canUploadAssets: true
+          }
+        }}
+        sdk={this.props.sdk}
+      />
+    );
+  };
+}
+
+init((sdk: FieldExtensionSDK) => {
+  sdk.window.startAutoResizer();
+  if (sdk.location.is(locations.LOCATION_DIALOG)) {
+    const dialogSdk = sdk as DialogExtensionSDK;
+    render(renderMarkdownDialog(dialogSdk as any), document.getElementById('root'));
+  } else {
+    render(<App sdk={sdk as FieldExtensionSDK} />, document.getElementById('root'));
+  }
+});
+
+/**
+ * By default, iframe of the extension is fully reloaded on every save of a source file.
+ * If you want to use HMR (hot module reload) instead of full reload, uncomment the following lines
+ */
+// if (module.hot) {
+//   module.hot.accept();
+// }
