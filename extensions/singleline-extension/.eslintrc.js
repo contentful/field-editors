acf9a4e9deeba938988a4e39b505fac801d80721
John Whiles <john.whiles@contentful.com>
2020-04-24 16:09:05 +0200

    feat: update scripts and lintrc in single line extension

--- extensions/singleline-extension/.eslintrc.js
+++ extensions/singleline-extension/.eslintrc.js
@@ -1,8 +1,8 @@
+const path = require('path');
+
 module.exports = {
-  extends: [
-    require.resolve('@contentful/eslint-config-extension/typescript'),
-    require.resolve('@contentful/eslint-config-extension/jest'),
-    require.resolve('@contentful/eslint-config-extension/jsx-a11y'),
-    require.resolve('@contentful/eslint-config-extension/react')
-  ]
+  extends: '../.eslintrc.js',
+  parserOptions: {
+    project: path.resolve('tsconfig.json')
+  }
 };
