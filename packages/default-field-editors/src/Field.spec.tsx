b2c8cf7a708d45b1f4b5485a35bfe4154f643f73
Danil Zakablukovskii <danil.zakablukovskii@contentful.com>
2020-08-21 15:55:53 +0200

    feat: added getOptions as a way to customize editor props

--- packages/default-field-editors/src/Field.spec.tsx
+++ packages/default-field-editors/src/Field.spec.tsx
@@ -4,10 +4,15 @@ import '@testing-library/jest-dom/extend-expect';
 import { createFakeFieldAPI, createFakeLocalesAPI } from '@contentful/field-editor-test-utils';
 import type { FieldExtensionSDK } from '@contentful/field-editor-shared';
 import { Field } from './Field';
+import { SingleEntryReferenceEditor } from '@contentful/field-editor-reference';
 
 configure({
   testIdAttribute: 'data-test-id',
 });
+
+jest.mock('@contentful/field-editor-reference', () => ({
+  SingleEntryReferenceEditor: jest.fn(() => <div>mock</div>),
+}));
 
 const [field] = createFakeFieldAPI();
 const sdk: FieldExtensionSDK = {
@@ -31,5 +36,26 @@ describe('Field', () => {
     );
 
     expect(queryByTestId('customEditor')).toBeInTheDocument();
+  });
+
+  it('renders with specified options', () => {
+    const options = {
+      entryLinkEditor: {
+        onAction: jest.fn(),
+        renderCustomCard: jest.fn(),
+      },
+    };
+    render(
+      <Field
+        sdk={sdk}
+        isInitiallyDisabled={false}
+        widgetId="entryLinkEditor"
+        getOptions={() => options}
+      />
+    );
+    expect(((SingleEntryReferenceEditor as unknown) as jest.Mock).mock.calls[0][0]).toMatchObject({
+      onAction: options.entryLinkEditor.onAction,
+      renderCustomCard: options.entryLinkEditor.renderCustomCard,
+    } as Partial<Parameters<typeof SingleEntryReferenceEditor>[0]>);
   });
 });
--- packages/default-field-editors/src/Field.tsx
+++ packages/default-field-editors/src/Field.tsx
@@ -26,6 +26,33 @@ import type { FieldExtensionSDK } from '@contentful/field-editor-shared';
 import '@contentful/field-editor-date/styles/styles.css';
 import type { WidgetType } from './types';
 
+type EditorOptions = {
+  multipleLine: Partial<Parameters<typeof MultipleLineEditor>[0]>;
+  boolean: Partial<Parameters<typeof BooleanEditor>[0]>;
+  entryCardEditor: Partial<Parameters<typeof SingleEntryReferenceEditor>[0]>;
+  objectEditor: Partial<Parameters<typeof JsonEditor>[0]>;
+  datePicker: Partial<Parameters<typeof DateEditor>[0]>;
+  locationEditor: Partial<Parameters<typeof LocationEditor>[0]>;
+  checkbox: Partial<Parameters<typeof CheckboxEditor>[0]>;
+  listInput: Partial<Parameters<typeof ListEditor>[0]>;
+  rating: Partial<Parameters<typeof RatingEditor>[0]>;
+  radio: Partial<Parameters<typeof RadioEditor>[0]>;
+  tagEditor: Partial<Parameters<typeof TagsEditor>[0]>;
+  numberEditor: Partial<Parameters<typeof NumberEditor>[0]>;
+  urlEditor: Partial<Parameters<typeof UrlEditor>[0]>;
+  slugEditor: Partial<Parameters<typeof SlugEditor>[0]>;
+  singleLine: Partial<Parameters<typeof SingleLineEditor>[0]>;
+  dropdown: Partial<Parameters<typeof DropdownEditor>[0]>;
+  entryLinkEditor: Partial<Parameters<typeof SingleEntryReferenceEditor>[0]>;
+  entryLinksEditor: Partial<Parameters<typeof MultipleEntryReferenceEditor>[0]>;
+  entryCardsEditor: Partial<Parameters<typeof MultipleEntryReferenceEditor>[0]>;
+  assetLinkEditor: Partial<Parameters<typeof SingleMediaEditor>[0]>;
+  assetLinksEditor: Partial<Parameters<typeof MultipleMediaEditor>[0]>;
+  assetGalleryEditor: Partial<Parameters<typeof MultipleMediaEditor>[0]>;
+  richTextEditor: Partial<Parameters<typeof RichTextEditor>[0]>;
+  markdown: Partial<Parameters<typeof MarkdownEditor>[0]>;
+};
+
 type FieldProps = {
   sdk: FieldExtensionSDK;
   widgetId: WidgetType;
@@ -35,6 +62,7 @@ type FieldProps = {
     sdk: FieldExtensionSDK,
     isInitiallyDisabled: boolean
   ) => JSX.Element | false;
+  getOptions?: (widgetId: WidgetType, sdk: FieldExtensionSDK) => Partial<EditorOptions>;
 };
 
 export const Field: React.FC<FieldProps> = ({
@@ -42,9 +70,19 @@ export const Field: React.FC<FieldProps> = ({
   widgetId,
   isInitiallyDisabled = false,
   renderFieldEditor,
+  getOptions,
 }: FieldProps) => {
   const field = sdk.field;
   const locales = sdk.locales;
+
+  if (renderFieldEditor) {
+    const customEditor = renderFieldEditor(widgetId, sdk, isInitiallyDisabled);
+    if (customEditor) {
+      return customEditor;
+    }
+  }
+
+  const options = getOptions ? getOptions(widgetId, sdk) : {};
   const referenceEditorParams =
     sdk.parameters && 'instance' in sdk.parameters
       ? sdk.parameters
@@ -55,13 +93,6 @@ export const Field: React.FC<FieldProps> = ({
           },
         };
 
-  if (renderFieldEditor) {
-    const customEditor = renderFieldEditor(widgetId, sdk, isInitiallyDisabled);
-    if (customEditor) {
-      return customEditor;
-    }
-  }
-
   switch (widgetId) {
     case 'multipleLine':
       return (
@@ -69,49 +100,122 @@ export const Field: React.FC<FieldProps> = ({
           field={field}
           locales={locales}
           isInitiallyDisabled={isInitiallyDisabled}
+          {...options[widgetId]}
         />
       );
     case 'boolean':
-      return <BooleanEditor field={field} isInitiallyDisabled={isInitiallyDisabled} />;
+      return (
+        <BooleanEditor
+          field={field}
+          isInitiallyDisabled={isInitiallyDisabled}
+          {...options[widgetId]}
+        />
+      );
     case 'objectEditor':
-      return <JsonEditor field={field} isInitiallyDisabled={isInitiallyDisabled} />;
+      return (
+        <JsonEditor
+          field={field}
+          isInitiallyDisabled={isInitiallyDisabled}
+          {...options[widgetId]}
+        />
+      );
     case 'datePicker':
-      return <DateEditor field={field} isInitiallyDisabled={isInitiallyDisabled} />;
+      return (
+        <DateEditor
+          field={field}
+          isInitiallyDisabled={isInitiallyDisabled}
+          {...options[widgetId]}
+        />
+      );
     case 'locationEditor':
-      return <LocationEditor field={field} isInitiallyDisabled={isInitiallyDisabled} />;
+      return (
+        <LocationEditor
+          field={field}
+          isInitiallyDisabled={isInitiallyDisabled}
+          {...options[widgetId]}
+        />
+      );
     case 'checkbox':
       return (
-        <CheckboxEditor field={field} locales={locales} isInitiallyDisabled={isInitiallyDisabled} />
+        <CheckboxEditor
+          field={field}
+          locales={locales}
+          isInitiallyDisabled={isInitiallyDisabled}
+          {...options[widgetId]}
+        />
       );
     case 'listInput':
       return (
-        <ListEditor field={field} locales={locales} isInitiallyDisabled={isInitiallyDisabled} />
+        <ListEditor
+          field={field}
+          locales={locales}
+          isInitiallyDisabled={isInitiallyDisabled}
+          {...options[widgetId]}
+        />
       );
     case 'rating':
-      return <RatingEditor field={field} isInitiallyDisabled={isInitiallyDisabled} />;
+      return (
+        <RatingEditor
+          field={field}
+          isInitiallyDisabled={isInitiallyDisabled}
+          {...options[widgetId]}
+        />
+      );
     case 'radio':
       return (
-        <RadioEditor field={field} locales={locales} isInitiallyDisabled={isInitiallyDisabled} />
+        <RadioEditor
+          field={field}
+          locales={locales}
+          isInitiallyDisabled={isInitiallyDisabled}
+          {...options[widgetId]}
+        />
       );
     case 'tagEditor':
-      return <TagsEditor field={field} isInitiallyDisabled={isInitiallyDisabled} />;
+      return (
+        <TagsEditor
+          field={field}
+          isInitiallyDisabled={isInitiallyDisabled}
+          {...options[widgetId]}
+        />
+      );
     case 'numberEditor':
-      return <NumberEditor field={field} isInitiallyDisabled={isInitiallyDisabled} />;
+      return (
+        <NumberEditor
+          field={field}
+          isInitiallyDisabled={isInitiallyDisabled}
+          {...options[widgetId]}
+        />
+      );
     case 'urlEditor':
-      return <UrlEditor field={field} isInitiallyDisabled={isInitiallyDisabled} />;
+      return (
+        <UrlEditor field={field} isInitiallyDisabled={isInitiallyDisabled} {...options[widgetId]} />
+      );
     case 'slugEditor':
-      return <SlugEditor field={field} baseSdk={sdk} isInitiallyDisabled={isInitiallyDisabled} />;
+      return (
+        <SlugEditor
+          field={field}
+          baseSdk={sdk}
+          isInitiallyDisabled={isInitiallyDisabled}
+          {...options[widgetId]}
+        />
+      );
     case 'singleLine':
       return (
         <SingleLineEditor
           field={field}
           locales={locales}
           isInitiallyDisabled={isInitiallyDisabled}
+          {...options[widgetId]}
         />
       );
     case 'dropdown':
       return (
-        <DropdownEditor field={field} locales={locales} isInitiallyDisabled={isInitiallyDisabled} />
+        <DropdownEditor
+          field={field}
+          locales={locales}
+          isInitiallyDisabled={isInitiallyDisabled}
+          {...options[widgetId]}
+        />
       );
     case 'entryLinkEditor': {
       return (
@@ -121,6 +225,7 @@ export const Field: React.FC<FieldProps> = ({
           isInitiallyDisabled={isInitiallyDisabled}
           hasCardEditActions={true}
           parameters={referenceEditorParams}
+          {...options[widgetId]}
         />
       );
     }
@@ -132,6 +237,7 @@ export const Field: React.FC<FieldProps> = ({
           isInitiallyDisabled={isInitiallyDisabled}
           hasCardEditActions={true}
           parameters={referenceEditorParams}
+          {...options[widgetId]}
         />
       );
     }
@@ -143,6 +249,7 @@ export const Field: React.FC<FieldProps> = ({
           isInitiallyDisabled={isInitiallyDisabled}
           hasCardEditActions={true}
           parameters={referenceEditorParams}
+          {...options[widgetId]}
         />
       );
     }
@@ -154,6 +261,7 @@ export const Field: React.FC<FieldProps> = ({
           isInitiallyDisabled={isInitiallyDisabled}
           hasCardEditActions={true}
           parameters={referenceEditorParams}
+          {...options[widgetId]}
         />
       );
     }
@@ -164,6 +272,7 @@ export const Field: React.FC<FieldProps> = ({
           sdk={sdk}
           isInitiallyDisabled={isInitiallyDisabled}
           parameters={referenceEditorParams}
+          {...options[widgetId]}
         />
       );
     }
@@ -174,6 +283,7 @@ export const Field: React.FC<FieldProps> = ({
           sdk={sdk}
           isInitiallyDisabled={isInitiallyDisabled}
           parameters={referenceEditorParams}
+          {...options[widgetId]}
         />
       );
     }
@@ -184,14 +294,27 @@ export const Field: React.FC<FieldProps> = ({
           sdk={sdk}
           isInitiallyDisabled={isInitiallyDisabled}
           parameters={referenceEditorParams}
+          {...options[widgetId]}
         />
       );
     }
     case 'richTextEditor': {
-      return <RichTextEditor sdk={sdk} isInitiallyDisabled={isInitiallyDisabled} />;
+      return (
+        <RichTextEditor
+          sdk={sdk}
+          isInitiallyDisabled={isInitiallyDisabled}
+          {...options[widgetId]}
+        />
+      );
     }
     case 'markdown': {
-      return <MarkdownEditor sdk={sdk} isInitiallyDisabled={isInitiallyDisabled} />;
+      return (
+        <MarkdownEditor
+          sdk={sdk}
+          isInitiallyDisabled={isInitiallyDisabled}
+          {...options[widgetId]}
+        />
+      );
     }
     default:
       return null;
