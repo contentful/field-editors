407270968737fc6a8065d2e1fcf63659dff879f8
Danil Zakablukovskii <danil.zakablukovskii@contentful.com>
2020-08-24 16:27:17 +0200

    feat: export EditorOptions type

--- packages/default-field-editors/src/Field.tsx
+++ packages/default-field-editors/src/Field.tsx
@@ -24,34 +24,7 @@ import { RichTextEditor } from '@contentful/field-editor-rich-text';
 import { MarkdownEditor } from '@contentful/field-editor-markdown';
 import type { FieldExtensionSDK } from '@contentful/field-editor-shared';
 import '@contentful/field-editor-date/styles/styles.css';
-import type { WidgetType } from './types';
-
-type EditorOptions = {
-  multipleLine: Partial<Parameters<typeof MultipleLineEditor>[0]>;
-  boolean: Partial<Parameters<typeof BooleanEditor>[0]>;
-  entryCardEditor: Partial<Parameters<typeof SingleEntryReferenceEditor>[0]>;
-  objectEditor: Partial<Parameters<typeof JsonEditor>[0]>;
-  datePicker: Partial<Parameters<typeof DateEditor>[0]>;
-  locationEditor: Partial<Parameters<typeof LocationEditor>[0]>;
-  checkbox: Partial<Parameters<typeof CheckboxEditor>[0]>;
-  listInput: Partial<Parameters<typeof ListEditor>[0]>;
-  rating: Partial<Parameters<typeof RatingEditor>[0]>;
-  radio: Partial<Parameters<typeof RadioEditor>[0]>;
-  tagEditor: Partial<Parameters<typeof TagsEditor>[0]>;
-  numberEditor: Partial<Parameters<typeof NumberEditor>[0]>;
-  urlEditor: Partial<Parameters<typeof UrlEditor>[0]>;
-  slugEditor: Partial<Parameters<typeof SlugEditor>[0]>;
-  singleLine: Partial<Parameters<typeof SingleLineEditor>[0]>;
-  dropdown: Partial<Parameters<typeof DropdownEditor>[0]>;
-  entryLinkEditor: Partial<Parameters<typeof SingleEntryReferenceEditor>[0]>;
-  entryLinksEditor: Partial<Parameters<typeof MultipleEntryReferenceEditor>[0]>;
-  entryCardsEditor: Partial<Parameters<typeof MultipleEntryReferenceEditor>[0]>;
-  assetLinkEditor: Partial<Parameters<typeof SingleMediaEditor>[0]>;
-  assetLinksEditor: Partial<Parameters<typeof MultipleMediaEditor>[0]>;
-  assetGalleryEditor: Partial<Parameters<typeof MultipleMediaEditor>[0]>;
-  richTextEditor: Partial<Parameters<typeof RichTextEditor>[0]>;
-  markdown: Partial<Parameters<typeof MarkdownEditor>[0]>;
-};
+import type { EditorOptions, WidgetType } from './types';
 
 type FieldProps = {
   sdk: FieldExtensionSDK;
--- packages/default-field-editors/src/types.ts
+++ packages/default-field-editors/src/types.ts
@@ -1,3 +1,26 @@
+import { MultipleLineEditor } from '@contentful/field-editor-multiple-line';
+import { BooleanEditor } from '@contentful/field-editor-boolean';
+import {
+  MultipleEntryReferenceEditor, MultipleMediaEditor,
+  SingleEntryReferenceEditor,
+  SingleMediaEditor
+} from '@contentful/field-editor-reference';
+import { JsonEditor } from '@contentful/field-editor-json';
+import { DateEditor } from '@contentful/field-editor-date';
+import { LocationEditor } from '@contentful/field-editor-location';
+import { CheckboxEditor } from '@contentful/field-editor-checkbox';
+import { ListEditor } from '@contentful/field-editor-list';
+import { RatingEditor } from '@contentful/field-editor-rating';
+import { RadioEditor } from '@contentful/field-editor-radio';
+import { TagsEditor } from '@contentful/field-editor-tags';
+import { NumberEditor } from '@contentful/field-editor-number';
+import { UrlEditor } from '@contentful/field-editor-url';
+import { SlugEditor } from '@contentful/field-editor-slug';
+import { SingleLineEditor } from '@contentful/field-editor-single-line';
+import { DropdownEditor } from '@contentful/field-editor-dropdown';
+import { RichTextEditor } from '@contentful/field-editor-rich-text';
+import { MarkdownEditor } from '@contentful/field-editor-markdown';
+
 export type WidgetType =
   | 'multipleLine'
   | 'boolean'
@@ -24,3 +47,30 @@ export type WidgetType =
   | 'richTextEditor'
   | 'markdown'
   | string;
+
+export type EditorOptions = {
+  multipleLine: Partial<Parameters<typeof MultipleLineEditor>[0]>;
+  boolean: Partial<Parameters<typeof BooleanEditor>[0]>;
+  entryCardEditor: Partial<Parameters<typeof SingleEntryReferenceEditor>[0]>;
+  objectEditor: Partial<Parameters<typeof JsonEditor>[0]>;
+  datePicker: Partial<Parameters<typeof DateEditor>[0]>;
+  locationEditor: Partial<Parameters<typeof LocationEditor>[0]>;
+  checkbox: Partial<Parameters<typeof CheckboxEditor>[0]>;
+  listInput: Partial<Parameters<typeof ListEditor>[0]>;
+  rating: Partial<Parameters<typeof RatingEditor>[0]>;
+  radio: Partial<Parameters<typeof RadioEditor>[0]>;
+  tagEditor: Partial<Parameters<typeof TagsEditor>[0]>;
+  numberEditor: Partial<Parameters<typeof NumberEditor>[0]>;
+  urlEditor: Partial<Parameters<typeof UrlEditor>[0]>;
+  slugEditor: Partial<Parameters<typeof SlugEditor>[0]>;
+  singleLine: Partial<Parameters<typeof SingleLineEditor>[0]>;
+  dropdown: Partial<Parameters<typeof DropdownEditor>[0]>;
+  entryLinkEditor: Partial<Parameters<typeof SingleEntryReferenceEditor>[0]>;
+  entryLinksEditor: Partial<Parameters<typeof MultipleEntryReferenceEditor>[0]>;
+  entryCardsEditor: Partial<Parameters<typeof MultipleEntryReferenceEditor>[0]>;
+  assetLinkEditor: Partial<Parameters<typeof SingleMediaEditor>[0]>;
+  assetLinksEditor: Partial<Parameters<typeof MultipleMediaEditor>[0]>;
+  assetGalleryEditor: Partial<Parameters<typeof MultipleMediaEditor>[0]>;
+  richTextEditor: Partial<Parameters<typeof RichTextEditor>[0]>;
+  markdown: Partial<Parameters<typeof MarkdownEditor>[0]>;
+};
