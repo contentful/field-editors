b016e89fcc30c5eadbdbcd7bf43f8c77aacbcf18
Yiotis Kaltsikis <giotiskl@users.noreply.github.com>
2021-03-04 15:55:47 +0100

    feat: allow rendering of custom missing entity card (#623)
    * feat: allow rendering of custom missing entity card
    
    * chore: remove unused ts-expect-error

--- packages/reference/src/index.tsx
+++ packages/reference/src/index.tsx
@@ -15,4 +15,9 @@ export {
 export { SingleMediaEditor, MultipleMediaEditor, WrappedAssetCard } from './assets';
 export { EntityProvider, useEntities } from './common/EntityStore';
 export type { CustomActionProps } from './common/ReferenceEditor';
-export type { CustomEntityCardProps, DefaultCardRenderer } from './common/customCardTypes';
+export type {
+  CustomEntityCardProps,
+  DefaultCardRenderer,
+  MissingEntityCardProps,
+  RenderCustomMissingEntityCard,
+} from './common/customCardTypes';
