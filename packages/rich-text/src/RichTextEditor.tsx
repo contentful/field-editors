f08dacb01e6d634a0739cf61950d04f370bba819
Mike Mitchell <mike.mitchell@contentful.com>
2021-05-06 17:32:55 +0200

    feat: move existing styles over to new component

--- packages/rich-text/src/RichTextEditor.tsx
+++ packages/rich-text/src/RichTextEditor.tsx
@@ -11,6 +11,8 @@ import {
 import { toContentfulDocument, toSlatejsDocument } from '@contentful/contentful-slatejs-adapter';
 import * as Contentful from '@contentful/rich-text-types';
 import { EntityProvider } from '@contentful/field-editor-reference';
+import { css, cx } from 'emotion';
+import tokens from '@contentful/forma-36-tokens';
 import { FieldExtensionSDK, FieldConnector } from '@contentful/field-editor-shared';
 import schema from './constants/Schema';
 import deepEquals from 'fast-deep-equal';
@@ -78,23 +80,84 @@ const ConnectedRichTextEditor = (props: ConnectedProps) => {
     return <Leaf {...props} />;
   }, []);
 
+  // TODO: Move into own css file?
+  const STYLE_EDITOR_BORDER = `1px solid ${tokens.colorElementDark}`;
+
+  const styles = {
+    root: css({
+      position: 'relative',
+    }),
+    editor: css({
+      borderRadius: `0 0 ${tokens.borderRadiusMedium} ${tokens.borderRadiusMedium}`,
+      border: STYLE_EDITOR_BORDER,
+      borderTop: 0,
+      padding: '20px',
+      fontSize: tokens.spacingM,
+      minHeight: '400px',
+      background: tokens.colorWhite,
+      outline: 'none',
+      whiteSpace: 'pre-wrap',
+      overflowWrap: 'break-word',
+      webkitUserModify: 'read-write-plaintext-only',
+      a: {
+        span: {
+          cursor: 'not-allowed',
+          '&:hover': {
+            cursor: 'not-allowed',
+          },
+        },
+      },
+    }),
+    hiddenToolbar: css({
+      borderTop: STYLE_EDITOR_BORDER,
+    }),
+    enabled: css({
+      background: tokens.colorWhite,
+      a: {
+        span: {
+          cursor: 'pointer',
+          '&:hover': {
+            cursor: 'pointer',
+          },
+        },
+      },
+    }),
+    disabled: css({
+      background: tokens.colorElementLightest,
+    }),
+  };
+
+  const classNames = cx(styles.editor);
+
+  // TODO: Fix props - possibly want to move Editable to its own wrapped component?
+  // const classNames = cx(
+  //   styles.editor,
+  //   this.props.minHeight !== undefined ? css({ minHeight: this.props.minHeight }) : undefined,
+  //   this.props.isDisabled ? styles.disabled : styles.enabled,
+  //   this.props.isToolbarHidden && styles.hiddenToolbar
+  // );
+
   return (
-    <Slate
-      editor={editor}
-      // TODO: normalize like in the webapp?
-      // cf. https://github.com/contentful/field-editors/blob/master/packages/rich-text/src/RichTextEditor.jsx#L69-L85
-      value={value}
-      onChange={(newValue) => {
-        setValue(newValue as CustomElement[]);
-        const doc = toContentfulDocument({ document: newValue, schema });
-        props.onChange?.(doc);
-      }}>
-      <Editable
-        renderElement={renderElement}
-        renderLeaf={renderLeaf}
-        onKeyDown={withEvents(editor)}
-      />
-    </Slate>
+    <div className={styles.root} data-test-id="rich-text-editor">
+      <div>Toolbar</div>
+      <Slate
+        editor={editor}
+        // TODO: normalize like in the webapp?
+        // cf. https://github.com/contentful/field-editors/blob/master/packages/rich-text/src/RichTextEditor.jsx#L69-L85
+        value={value}
+        onChange={(newValue) => {
+          setValue(newValue as CustomElement[]);
+          const doc = toContentfulDocument({ document: newValue, schema });
+          props.onChange?.(doc);
+        }}>
+        <Editable
+          className={classNames}
+          renderElement={renderElement}
+          renderLeaf={renderLeaf}
+          onKeyDown={withEvents(editor)}
+        />
+      </Slate>
+    </div>
   );
 };
 
