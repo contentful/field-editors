8210ed181555f995492495a66c51d2c57fe27b4f
Chris Sloop <chris.sloop@contentful.com>
2021-05-07 16:40:11 +0200

    chore: remove needless comment

--- packages/rich-text/src/constants/Schema.ts
+++ packages/rich-text/src/constants/Schema.ts
@@ -104,7 +104,6 @@ export default {
         }
       ],
       normalize: (editor, error) => {
-        // TODO: still a relevant error code from Slate?
         if (error.code === 'child_type_invalid') {
           return editor.unwrapBlockByKey(error.node.key, BLOCKS.QUOTE);
         }
