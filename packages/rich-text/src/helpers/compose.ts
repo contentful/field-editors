1bfe08da8e92257ccb39d39e0e9bfd19a31bd974
Cezar Sampaio <cezar.sampaio@contentful.com>
2021-05-07 17:35:20 +0200

    chore: use lodash flow instead of compose

--- packages/rich-text/src/helpers/compose.ts
+++
@@ -1,6 +0,0 @@
-export function compose(...fns: Function[]) {
-  return fns.reduceRight(
-    (prevFn, nextFn) => (...args: unknown[]) => nextFn(prevFn(...args)),
-    (value: unknown) => value
-  );
-}
