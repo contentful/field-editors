d67a5d255c05f5f95386590cd5fa5b5842a543cd
Cezar Sampaio <cezar.sampaio@contentful.com>
2021-05-11 13:00:12 +0200

    chore: set leaf component props

--- packages/rich-text/src/plugins/Leaf/index.tsx
+++ packages/rich-text/src/plugins/Leaf/index.tsx
@@ -1,28 +1,38 @@
 import * as React from 'react';
 import { RenderLeafProps } from 'slate-react';
+import { Text } from 'slate';
 
 import { Bold } from '../Bold';
 import { Code } from '../Code';
 import { Italic } from '../Italic';
 import { Underline } from '../Underline';
 
-export function Leaf(props: RenderLeafProps) {
+interface Props extends RenderLeafProps {
+  leaf: Text & {
+    bold?: boolean;
+    code?: boolean;
+    italic?: boolean;
+    underline?: boolean;
+  };
+}
+
+export function Leaf(props: Props) {
   const { attributes, leaf } = props;
   let { children } = props;
 
-  if ('bold' in leaf) {
+  if (leaf.bold) {
     children = <Bold {...props}>{children}</Bold>;
   }
 
-  if ('code' in leaf) {
+  if (leaf.code) {
     children = <Code {...props}>{children}</Code>;
   }
 
-  if ('italic' in leaf) {
+  if (leaf.italic) {
     children = <Italic {...props}>{children}</Italic>;
   }
 
-  if ('underline' in leaf) {
+  if (leaf.underline) {
     children = <Underline {...props}>{children}</Underline>;
   }
 
