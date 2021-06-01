d6af0d9d91148fe323c93af386582286586b6341
anho <me@anho.github.com>
2021-06-01 10:07:22 +0200

    chore: change paragraphs

--- packages/markdown/src/components/VoidElements/VoidElementsWarning.tsx
+++ packages/markdown/src/components/VoidElements/VoidElementsWarning.tsx
@@ -26,13 +26,14 @@ export function VoidElementsWarning() {
   return (
     <Note noteType="warning" hasCloseButton={false} className={styles.note} testId="markdown-void-elements-warning">
       <Paragraph className={styles.paragraph}>
-        Your markdown contains <TextLink linkType="primary" icon="ExternalLink" iconPosition="right">void
-        elements</TextLink> that causes the preview to skip those parts. This indicates incorrect usage of one or more
-        of the following HTML elements: area, base, br, col, command, embed, hr, img, input, keygen, link, meta, param,
-        source, track, wbr.
+        Your markdown contains <TextLink linkType="primary" icon="ExternalLink" iconPosition="right"
+                                         href="https://www.w3.org/TR/2011/WD-html-markup-20110113/syntax.html#void-element">void
+        elements</TextLink> that causes the preview to skip those parts.
       </Paragraph>
       <Paragraph className={styles.paragraph}>
-        In order to properly display the preview fix the elements or replace them with alternatives.
+        This indicates incorrect usage of one or more of the following HTML elements: area, base, br, col, command,
+        embed, hr, img, input, keygen, link, meta, param, source, track, wbr.In order to properly display the preview
+        fix the elements or replace them with alternatives.
       </Paragraph>
     </Note>
   );
