2c7bdd1b6ca4018516d8675f56a161e67d913bed
alexdonets <alexander.donets@contentful.com>
2021-06-23 13:29:24 +0200

    chore: migrate rich text extension to cca

--- extensions/rich-text-extension/src/index.tsx
+++ extensions/rich-text-extension/src/index.tsx
@@ -4,7 +4,7 @@ import { init, FieldExtensionSDK, locations } from '@contentful/app-sdk';
 import '@contentful/forma-36-react-components/dist/styles.css';
 import '@contentful/forma-36-fcss/dist/styles.css';
 import './index.css';
-import { RichTextEditor, renderRichTextDialog } from '../../../packages/rich-text/src/index';
+import { RichTextEditor, renderRichTextDialog } from '@contentful/field-editor-rich-text';
 
 init((sdk: FieldExtensionSDK) => {
   sdk.window.startAutoResizer();
