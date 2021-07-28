2d2de83f4023acd78e4d12cf41e24a030ed05485
Cezar Sampaio <cezar.sampaio@contentful.com>
2021-07-28 17:34:30 +0200

    chore: add suport for hr deserialization

--- packages/rich-text/src/helpers/deserializer.ts
+++ packages/rich-text/src/helpers/deserializer.ts
@@ -4,7 +4,7 @@ import {
   Deserialize,
   GetNodeDeserializerRule,
 } from '@udecode/slate-plugins-core';
-import { getLeafDeserializer } from '@udecode/slate-plugins-common';
+import { getLeafDeserializer, getElementDeserializer } from '@udecode/slate-plugins-common';
 
 export function deserializeLeaf(type: string, rules: GetNodeDeserializerRule[]): Deserialize {
   return function (editor: SPEditor) {
@@ -12,6 +12,19 @@ export function deserializeLeaf(type: string, rules: GetNodeDeserializerRule[]):
 
     return {
       leaf: getLeafDeserializer({
+        rules,
+        ...pluginOptions,
+      }),
+    };
+  };
+}
+
+export function deserializeElement(type: string, rules: GetNodeDeserializerRule[]): Deserialize {
+  return function (editor: SPEditor) {
+    const pluginOptions = getSlatePluginOptions(editor, type);
+
+    return {
+      element: getElementDeserializer({
         rules,
         ...pluginOptions,
       }),
