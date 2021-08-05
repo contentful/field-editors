3237f6ce51cf5d29880e641858a61ffcd9c61fa4
Andreas Hörnicke <andreas.hoernicke@gmail.com>
2021-08-05 16:30:30 +0200

    feat: 🎸 add char counter (#795)
    ✅ Closes: #491

--- packages/multiple-line/src/MultipleLineEditor.tsx
+++ packages/multiple-line/src/MultipleLineEditor.tsx
@@ -1,6 +1,13 @@
 import * as React from 'react';
 import { Textarea } from '@contentful/forma-36-react-components';
-import { FieldAPI, LocalesAPI, FieldConnector } from '@contentful/field-editor-shared';
+import {
+  FieldAPI,
+  LocalesAPI,
+  FieldConnector,
+  CharCounter,
+  CharValidation,
+  ConstraintsUtils
+} from '@contentful/field-editor-shared';
 import * as styles from './styles';
 
 export interface MultipleLineEditorProps {
@@ -8,6 +15,11 @@ export interface MultipleLineEditorProps {
    * is the field disabled initially
    */
   isInitiallyDisabled: boolean;
+
+  /**
+   * whether char validation should be shown or not
+   */
+  withCharValidation: boolean;
 
   /**
    * sdk.field
@@ -21,12 +33,14 @@ export interface MultipleLineEditorProps {
 }
 
 export function MultipleLineEditor(props: MultipleLineEditorProps) {
-  const { field, locales } = props;
+  const { field, locales, isInitiallyDisabled, withCharValidation } = props;
 
+  const constraints = ConstraintsUtils.fromFieldValidations(field.validations, field.type as 'Text');
+  const checkConstraint = ConstraintsUtils.makeChecker(constraints);
   const direction = locales.direction[field.locale] || 'ltr';
 
   return (
-    <FieldConnector<string> field={field} isInitiallyDisabled={props.isInitiallyDisabled}>
+    <FieldConnector<string> field={field} isInitiallyDisabled={isInitiallyDisabled}>
       {({ errors, disabled, value, setValue }) => {
         return (
           <div data-test-id="multiple-line-editor">
@@ -41,6 +55,17 @@ export function MultipleLineEditor(props: MultipleLineEditorProps) {
                 setValue(e.target.value);
               }}
             />
+            {withCharValidation && (
+              <div className={styles.validationRow}>
+                <CharCounter value={value || ''} checkConstraint={checkConstraint} />
+                <CharValidation constraints={constraints} />
+              </div>
+            )}
+            {!withCharValidation && (
+              <div className={styles.validationRow}>
+                <CharCounter value={value || ''} checkConstraint={() => true} />
+              </div>
+            )}
           </div>
         );
       }}
@@ -50,4 +75,5 @@ export function MultipleLineEditor(props: MultipleLineEditorProps) {
 
 MultipleLineEditor.defaultProps = {
   isInitiallyDisabled: true,
+  withCharValidation: true,
 };
