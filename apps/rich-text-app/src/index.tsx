5810b6e766cfe3ea16bd3ad09ce5763c693e5d86
Daniel A. R. Werner <daniel.a.r.werner@gmail.com>
2021-08-23 10:27:17 +0200

    feat: 🎸 add missing public exports
    Adds public rich text package exports `ConnectedRichTextEditor`,
    `openRichTextDialog` and `renderRichTextDialog`.

--- apps/rich-text-app/src/index.tsx
+++ apps/rich-text-app/src/index.tsx
@@ -1,17 +1,17 @@
 import * as React from 'react';
 import { render } from 'react-dom';
-import { init, FieldExtensionSDK, locations } from '@contentful/app-sdk';
+import { init, DialogExtensionSDK, FieldExtensionSDK, locations } from '@contentful/app-sdk';
 import '@contentful/forma-36-react-components/dist/styles.css';
 import '@contentful/forma-36-fcss/dist/styles.css';
 import './index.css';
 import { RichTextEditor, renderRichTextDialog } from '@contentful/field-editor-rich-text';
 
-init((sdk: FieldExtensionSDK) => {
+init((sdk: FieldExtensionSDK|DialogExtensionSDK) => {
   sdk.window.startAutoResizer();
   if (sdk.location.is(locations.LOCATION_DIALOG)) {
-    render(renderRichTextDialog(sdk), document.getElementById('root'));
+    render(renderRichTextDialog(sdk as DialogExtensionSDK), document.getElementById('root'));
   } else {
-    render(<RichTextEditor sdk={sdk} />, document.getElementById('root'));
+    render(<RichTextEditor sdk={sdk as FieldExtensionSDK} isInitiallyDisabled={true} />, document.getElementById('root'));
   }
 });
 
--- packages/rich-text/src/index.tsx
+++ packages/rich-text/src/index.tsx
@@ -1,1 +1,5 @@
 export { default as RichTextEditor } from './RichTextEditor';
+export { ConnectedRichTextEditor } from './RichTextEditor';
+
+export { openRichTextDialog } from './dialogs/openRichTextDialog';
+export { renderRichTextDialog } from './dialogs/renderRichTextDialog';
