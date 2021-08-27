b7d33e3a13dc62204abb13221dac1f7554f65240
bgutsol <bohdan.hutsol@contentful.com>
2021-08-27 14:20:21 +0300

    chore(Note): v4 Note adoption

--- packages/_shared/src/PredefinedValuesError.tsx
+++ packages/_shared/src/PredefinedValuesError.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Note } from '@contentful/forma-36-react-components';
+import { Note } from '@contentful/f36-components';
 
 export function PredefinedValuesError() {
   return (
-    <Note noteType="warning" testId="predefined-values-warning">
+    <Note variant="warning" testId="predefined-values-warning">
       The widget failed to initialize. You can fix the problem by providing predefined values under
       the validations tab in the field settings.
     </Note>
