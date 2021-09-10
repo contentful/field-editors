c1a1f77d556b6ea3a1d2d0fe9512c9fb835ecd60
Cezar Sampaio <cezar.sampaio@contentful.com>
2021-09-10 13:04:54 +0200

    refactor: use exit break plugin instead (#839)

--- packages/rich-text/src/plugins/InsertBeforeFirstVoidBlock/index.tsx
+++ packages/rich-text/src/plugins/InsertBeforeFirstVoidBlock/index.tsx
@@ -1,40 +1,17 @@
-import { KeyboardEvent } from 'react';
-import { PlatePlugin, SPEditor } from '@udecode/plate-core';
-import {
-  isFirstChild,
-  isVoid,
-  getElementFromCurrentSelection,
-  moveToThePreviousLine,
-} from '../../helpers/editor';
-import { Transforms, Path } from 'slate';
-import { BLOCKS } from '@contentful/rich-text-types';
+import { PlatePlugin } from '@udecode/plate-core';
+import { isFirstChild } from '../../helpers/editor';
+import { ExitBreakPluginOptions, createExitBreakPlugin } from '@udecode/plate-break';
 
 export function createInsertBeforeFirstVoidBlockPlugin(): PlatePlugin {
-  return {
-    onKeyDown: function (editor: SPEditor) {
-      return (event: KeyboardEvent) => {
-        const isEnter = event.key === 'Enter';
-        const [currentElement, currentPath] = getElementFromCurrentSelection(editor);
-        const isFirst = isFirstChild(currentPath as Path);
-
-        if (isEnter && isFirst && isVoid(editor, currentElement)) {
-          const paragraph = {
-            type: BLOCKS.PARAGRAPH,
-            data: {},
-            children: [{ text: '' }],
-          };
-
-          // It adds two paragraphs outside a timeout and after moving the cursor to the previous line, not sure why...
-          setTimeout(() => {
-            Transforms.insertNodes(editor, paragraph, { at: currentPath as Path });
-            moveToThePreviousLine(editor);
-          }, 0);
-
-          return true; // To prevent the next handler from running
-        }
-
-        return false; // something like next()
-      };
-    },
+  const optionsExitBreakPlugin: ExitBreakPluginOptions = {
+    rules: [
+      {
+        hotkey: 'enter',
+        before: true,
+        query: { filter: ([node, path]) => isFirstChild(path) && !!node.isVoid },
+      },
+    ],
   };
+
+  return createExitBreakPlugin(optionsExitBreakPlugin);
 }
