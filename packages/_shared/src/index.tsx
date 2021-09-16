f26c481cc5abf275acb4e4a65b462be487708117
Cezar Sampaio <cezar.sampaio@contentful.com>
2021-09-16 17:00:32 +0200

    feat: export shared util to validate images (#846)

--- packages/_shared/src/index.tsx
+++ packages/_shared/src/index.tsx
@@ -32,4 +32,6 @@ export { entityHelpers };
 import * as ConstraintsUtils from './utils/constraints';
 export { ConstraintsUtils };
 
+export { isValidImage } from './utils/isValidImage';
+
 export { Entry, File, Asset } from './typesEntity';
