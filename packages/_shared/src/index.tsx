80efa7e4d906c9f98b2ddef6462b3e79289fb010
Cezar Sampaio <cezar.sampaio@contentful.com>
2021-09-17 13:57:10 +0200

    feat: add shortenStorageUnit util to shared

--- packages/_shared/src/index.tsx
+++ packages/_shared/src/index.tsx
@@ -34,4 +34,6 @@ export { ConstraintsUtils };
 
 export { isValidImage } from './utils/isValidImage';
 
+export { shortenStorageUnit, toLocaleString } from './utils/shortenStorageUnit';
+
 export { Entry, File, Asset } from './typesEntity';
