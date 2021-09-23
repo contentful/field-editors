56b83dec97ce277a421abfbf80198ac5c0a23e4b
Alex Suevalov <alex.suevalov@contentful.com>
2021-09-23 12:51:45 +0100

    chore: adjust value

--- cypress/integration/DateEditor.spec.ts
+++ cypress/integration/DateEditor.spec.ts
@@ -46,7 +46,7 @@ describe('Date Editor', () => {
   const openPage = () => {
     cy.visit('/date');
     cy.findByTestId('date-editor-integration-test').should('be.visible');
-    cy.wait(150);
+    cy.wait(200);
   };
 
   beforeEach(() => {
