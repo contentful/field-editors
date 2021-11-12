19505d3d58548d43a3469d484a97b40eaf743911
Luca Bianconi <71259950+IBratWurst@users.noreply.github.com>
2021-11-12 16:40:21 +0100

    fix: deserialize different lines as multiple paragraphs (#915)

--- packages/rich-text/src/plugins/Paragraph/index.tsx
+++ packages/rich-text/src/plugins/Paragraph/index.tsx
@@ -4,10 +4,11 @@ import { ELEMENT_PARAGRAPH } from '@udecode/plate-paragraph';
 import { BLOCKS } from '@contentful/rich-text-types';
 import tokens from '@contentful/f36-tokens';
 import { RenderElementProps } from 'slate-react';
-import { PlatePlugin, getRenderElement } from '@udecode/plate-core';
+import { PlatePlugin, getRenderElement, SPEditor } from '@udecode/plate-core';
 import { getToggleElementOnKeyDown } from '@udecode/plate-common';
 import { CustomSlatePluginOptions } from '../../types';
 import { deserializeElement } from '../../helpers/deserializer';
+import { Element, Node, Transforms } from 'slate';
 
 const styles = {
   [BLOCKS.PARAGRAPH]: css`
@@ -24,18 +25,65 @@ export function Paragraph(props: RenderElementProps) {
   );
 }
 
+function isEmbed(element: HTMLElement) {
+  return (
+    element.hasAttribute('data-embedded-entity-inline-id') ||
+    element.hasAttribute('data-entity-type')
+  );
+}
+
+function isEmpty(element: HTMLElement) {
+  return element.textContent === '';
+}
+
 export function createParagraphPlugin(): PlatePlugin {
   const elementKeys: string[] = [ELEMENT_PARAGRAPH, BLOCKS.PARAGRAPH];
+
+  const deserializer = deserializeElement(BLOCKS.PARAGRAPH, [
+    {
+      nodeNames: ['P', 'DIV'],
+    },
+  ]);
 
   return {
     renderElement: getRenderElement(elementKeys),
     pluginKeys: elementKeys,
     onKeyDown: getToggleElementOnKeyDown(BLOCKS.PARAGRAPH),
-    deserialize: deserializeElement(BLOCKS.PARAGRAPH, [
-      {
-        nodeNames: ['P'],
-      },
-    ]),
+    deserialize: (editor: SPEditor) => {
+      const { element, ...rest } = deserializer(editor);
+      return {
+        ...rest,
+        element: element?.map((deserializeNode) => ({
+          ...deserializeNode,
+          deserialize: (el: HTMLElement) => {
+            if (isEmpty(el) || isEmbed(el)) {
+              return;
+            }
+            return deserializeNode.deserialize(el);
+          },
+        })),
+      };
+    },
+    withOverrides: (editor) => {
+      const { normalizeNode } = editor;
+      editor.normalizeNode = (entry) => {
+        const [node, path] = entry;
+        // If the element is a paragraph, ensure its children are valid.
+        if (Element.isElement(node) && node.type === BLOCKS.PARAGRAPH) {
+          for (const [child, childPath] of Node.children(editor, path)) {
+            if (Element.isElement(child) && !editor.isInline(child)) {
+              Transforms.unwrapNodes(editor, {
+                at: childPath,
+              });
+              return;
+            }
+          }
+        }
+        // Fall back to the original `normalizeNode` to enforce other constraints.
+        normalizeNode(entry);
+      };
+      return editor;
+    },
   };
 }
 
