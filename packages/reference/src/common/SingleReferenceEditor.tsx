295254607c7c56cb251baf68ec9070fc64c1c918
Jesper Håkansson <jesper.hakansson@konsult.atg.se>
2021-11-18 08:11:25 +0100

    fix: Be able to use SingleMediaEditor with JSON object field

--- packages/reference/src/common/SingleReferenceEditor.tsx
+++ packages/reference/src/common/SingleReferenceEditor.tsx
@@ -81,7 +81,7 @@ export function SingleReferenceEditor(
           <Editor
             {...props}
             key={`${externalReset}-reference`}
-            entityId={value ? value.sys.id : ''}
+            entityId={value ? value?.sys?.id : ''}
             isDisabled={disabled}
             setValue={setValue}
             allContentTypes={allContentTypes}
