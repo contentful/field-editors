4a12c78df13c9e751c311f7a29a3ecc7f24c57e2
Cezar Sampaio <cezar.sampaio@contentful.com>
2021-12-15 10:30:51 +0100

    fix: enter on void elements only on root level

--- packages/rich-text/src/plugins/InsertBeforeFirstVoidBlock/index.tsx
+++ packages/rich-text/src/plugins/InsertBeforeFirstVoidBlock/index.tsx
@@ -1,6 +1,6 @@
 import { PlatePlugin } from '@udecode/plate-core';
 import { ExitBreakPluginOptions, createExitBreakPlugin } from '@udecode/plate-break';
-import { isFirstChild } from '../../helpers/editor';
+import { isFirstChild, isRootLevel } from '../../helpers/editor';
 
 export function createInsertBeforeFirstVoidBlockPlugin(): PlatePlugin {
   const optionsExitBreakPlugin: ExitBreakPluginOptions = {
@@ -8,11 +8,15 @@ export function createInsertBeforeFirstVoidBlockPlugin(): PlatePlugin {
       {
         hotkey: 'enter',
         before: true,
-        query: { filter: ([node, path]) => isFirstChild(path) && !!node.isVoid },
+        query: {
+          filter: ([node, path]) => isRootLevel(path) && isFirstChild(path) && !!node.isVoid,
+        },
       },
       {
         hotkey: 'enter',
-        query: { filter: ([node, path]) => !isFirstChild(path) && !!node.isVoid },
+        query: {
+          filter: ([node, path]) => !isFirstChild(path) && !!node.isVoid,
+        },
       },
     ],
   };
