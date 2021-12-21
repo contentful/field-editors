1b36f1f9e79c7943bc61ef878b9f471292b2adbb
Ahmed T. Ali <ah.tajelsir@gmail.com>
2021-12-21 23:41:49 +0100

    refactor: migrate DragAndDrop plugin

--- packages/rich-text/src/plugins/DragAndDrop/index.tsx
+++ packages/rich-text/src/plugins/DragAndDrop/index.tsx
@@ -28,6 +28,7 @@ export function createDragAndDropPlugin(): PlatePlugin {
   };
 
   return {
+    key: 'drag-and-drop',
     withOverrides: (editor) => {
       const { normalizeNode } = editor;
 
@@ -63,33 +64,35 @@ export function createDragAndDropPlugin(): PlatePlugin {
       return editor;
     },
 
-    // If true, the next handlers will be skipped.
-    onDrop: (editor) => (event) => {
-      const [draggingBlock] = Array.from(
-        getNodes(editor, {
-          match: (node) => DRAGGABLE_TYPES.includes(node?.type),
-        })
-      );
-      if (!draggingBlock) return false;
+    handlers: {
+      // If true, the next handlers will be skipped.
+      onDrop: (editor) => (event) => {
+        const [draggingBlock] = Array.from(
+          getNodes(editor, {
+            match: (node) => DRAGGABLE_TYPES.includes(node?.type),
+          })
+        );
+        if (!draggingBlock) return false;
 
-      const [draggingNode] = draggingBlock;
+        const [draggingNode] = draggingBlock;
 
-      if (!event.nativeEvent.target) return false;
+        if (!event.nativeEvent.target) return false;
 
-      // TODO: looking up for html nodes is not the best solution and it won't scale, we need to find a way to know the dropping target slate element
-      const dropDisallowed = getParents(event.nativeEvent.target as Node).some((node) => {
-        return ON_DROP_ALLOWED_TYPES[node.nodeName]
-          ? !ON_DROP_ALLOWED_TYPES[node.nodeName]?.includes(draggingNode.type)
-          : false;
-      });
+        // TODO: looking up for html nodes is not the best solution and it won't scale, we need to find a way to know the dropping target slate element
+        const dropDisallowed = getParents(event.nativeEvent.target as Node).some((node) => {
+          return ON_DROP_ALLOWED_TYPES[node.nodeName]
+            ? !ON_DROP_ALLOWED_TYPES[node.nodeName]?.includes(draggingNode.type)
+            : false;
+        });
 
-      if (!dropDisallowed) {
-        // Move the drop event to a new undo batch mitigating the bug where undo not only moves it back,
-        // but also undoes a previous action: https://github.com/ianstormtaylor/slate/issues/4694
-        editor.history.undos.push([]);
-      }
+        if (!dropDisallowed) {
+          // Move the drop event to a new undo batch mitigating the bug where undo not only moves it back,
+          // but also undoes a previous action: https://github.com/ianstormtaylor/slate/issues/4694
+          editor.history.undos.push([]);
+        }
 
-      return dropDisallowed;
+        return dropDisallowed;
+      },
     },
   };
 }
