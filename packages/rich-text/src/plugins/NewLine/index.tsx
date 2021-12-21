489f5a1f57f525b50c1bba83586ee1be5635ca87
Ahmed T. Ali <ah.tajelsir@gmail.com>
2021-12-21 23:39:54 +0100

    refactor: migrate Newline plugin

--- packages/rich-text/src/plugins/NewLine/index.tsx
+++ packages/rich-text/src/plugins/NewLine/index.tsx
@@ -3,19 +3,22 @@ import { PlatePlugin, PlateEditor } from '@udecode/plate-core';
 
 export function createNewLinePlugin(): PlatePlugin {
   return {
-    onKeyDown: function (editor: PlateEditor) {
-      return (event: KeyboardEvent) => {
-        const isEnter = event.key === 'Enter';
-        const isShift = event.shiftKey;
+    key: 'newline',
+    handlers: {
+      onKeyDown: function (editor: PlateEditor) {
+        return (event: KeyboardEvent) => {
+          const isEnter = event.key === 'Enter';
+          const isShift = event.shiftKey;
 
-        if (isEnter && isShift) {
-          event.preventDefault();
-          editor.insertText('\n');
-          return true; // To prevent the next handler from running
-        }
+          if (isEnter && isShift) {
+            event.preventDefault();
+            editor.insertText('\n');
+            return true; // To prevent the next handler from running
+          }
 
-        return false; // something like next()
-      };
+          return false; // something like next()
+        };
+      },
     },
   };
 }
