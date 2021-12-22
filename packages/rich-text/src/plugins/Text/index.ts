1d57519131d102258653d24c6183959970ef6d6b
Ahmed T. Ali <ah.tajelsir@gmail.com>
2021-12-22 19:07:26 +0100

    fix: normalize stranded text nodes

--- packages/rich-text/src/plugins/Text/index.ts
+++ packages/rich-text/src/plugins/Text/index.ts
@@ -1,7 +1,7 @@
-import { Editor, Ancestor, Transforms } from 'slate';
+import { Editor, Ancestor, Transforms, Text } from 'slate';
 import { PlatePlugin } from '@udecode/plate-core';
-import { isAncestorEmpty } from '@udecode/plate-core';
-import { TEXT_CONTAINERS, BLOCKS } from '@contentful/rich-text-types';
+import { isAncestorEmpty, getParent } from '@udecode/plate-core';
+import { TEXT_CONTAINERS, INLINES, BLOCKS } from '@contentful/rich-text-types';
 import { CustomElement } from '../../types';
 
 // TODO: move the logic to the appropriate element plugin(s)
@@ -9,7 +9,35 @@ export function createTextPlugin(): PlatePlugin {
   return {
     key: 'TextPlugin',
     withOverrides: (editor) => {
-      const { deleteForward } = editor;
+      const { deleteForward, normalizeNode } = editor;
+
+      editor.normalizeNode = (entry) => {
+        const [node, path] = entry;
+
+        if (!Text.isText(node)) {
+          return normalizeNode(entry);
+        }
+
+        const parent = getParent(editor, path)?.[0];
+
+        const isTextAllowed =
+          parent &&
+          (TEXT_CONTAINERS.includes(parent.type) || Object.values(INLINES).includes(parent.type));
+
+        // Force text elements to be wrapped in a text container
+        // This will be enforced by sanitizeSlateDoc() but the internal
+        // editor value can be different. It can lead, for example, to bugs like:
+        // Pressing ENTER on a table cell creates another column 🤷‍♂️
+        //
+        // cf. https://github.com/ianstormtaylor/slate/issues/2206
+        if (!isTextAllowed) {
+          const paragraph = { type: BLOCKS.PARAGRAPH, data: {}, children: [] };
+          Transforms.wrapNodes(editor, paragraph, { at: path });
+          return;
+        }
+
+        normalizeNode(entry);
+      };
 
       // When pressing delete instead of backspace
       editor.deleteForward = (unit) => {
