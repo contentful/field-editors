aaacb2629bd93eb47abd1752ae36cb359f5e88b2
Ahmed T. Ali <ah.tajelsir@gmail.com>
2022-01-10 11:14:10 +0100

    fix: select embedded blocks & HR on backspace (#982)
    * fix: select embedded blocks & HR on backspace
    
    * fix: include inline entries as well

---
+++ packages/rich-text/src/plugins/SelectOnBackspace/createSelectOnBackspacePlugin.ts
@@ -0,0 +1,13 @@
+import { BLOCKS, INLINES } from '@contentful/rich-text-types';
+import { createSelectOnBackspacePlugin as createDefaultSelectPlugin } from '@udecode/plate-select';
+
+import { RichTextPlugin } from '../../types';
+
+export const createSelectOnBackspacePlugin = (): RichTextPlugin =>
+  createDefaultSelectPlugin({
+    options: {
+      query: {
+        allow: [BLOCKS.EMBEDDED_ASSET, BLOCKS.EMBEDDED_ENTRY, BLOCKS.HR, INLINES.EMBEDDED_ENTRY],
+      },
+    },
+  });
