eb0b80f60654f3dfe9e3bcf9d6e8f520069771ae
Ahmed T. Ali <ah.tajelsir@gmail.com>
2022-01-25 17:32:10 +0100

    fix: handle more edge cases in insertListItem (#1005)

--- packages/rich-text/src/plugins/List/insertListBreak.ts
+++ packages/rich-text/src/plugins/List/insertListBreak.ts
@@ -4,6 +4,7 @@
  *
  * See: https://github.com/udecode/plate/blob/main/packages/nodes/list
  */
+import { BLOCKS, TEXT_CONTAINERS } from '@contentful/rich-text-types';
 import {
   ELEMENT_DEFAULT,
   getPluginType,
@@ -26,11 +27,14 @@ const listBreak = (editor: PlateEditor): boolean => {
   if (res) {
     const { list, listItem } = res;
 
-    // FIXME: take void children into account
-    // https://slate-js.slack.com/archives/C013QHXSCG1/p1642177267070200
-    //
+    const childNode = listItem[0].children[0];
+
     // If selected li is empty, move it up.
-    if (isBlockAboveEmpty(editor)) {
+    if (
+      isBlockAboveEmpty(editor) &&
+      listItem[0].children.length === 1 &&
+      TEXT_CONTAINERS.includes(childNode.type as BLOCKS)
+    ) {
       moved = moveListItemUp(editor, {
         list,
         listItem,
