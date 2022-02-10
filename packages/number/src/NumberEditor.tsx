716f1305a46d7b8513c7bfda696e50d794d06190
denkristoffer <denkristoffer@users.noreply.github.com>
2022-02-10 19:17:39 +0100

    fix(number-editor): use "text" input that syncs numeric value when valid
    Due to browser implementations React is unable to fully control inputs of `type="number"`.
    This commit changes `NumberEditor` to `type="text"` to avoid these issues. We keep a local string
    input value that we then convert to a number when it's actually valid.

--- packages/number/src/NumberEditor.tsx
+++ packages/number/src/NumberEditor.tsx
@@ -1,5 +1,9 @@
 import * as React from 'react';
-import { FieldAPI, FieldConnector } from '@contentful/field-editor-shared';
+import {
+  FieldAPI,
+  FieldConnector,
+  FieldConnectorChildProps,
+} from '@contentful/field-editor-shared';
 import { parseNumber } from './parseNumber';
 
 import { TextInput } from '@contentful/f36-components';
@@ -26,37 +30,80 @@ function getRangeFromField(field: FieldAPI): RangeValidation {
   return result ? result.range : {};
 }
 
+function valueToString(value: InnerNumberEditorProps['value']) {
+  return value === undefined ? '' : String(value);
+}
+
+type InnerNumberEditorProps = Pick<
+  FieldConnectorChildProps<number>,
+  'disabled' | 'errors' | 'setValue' | 'value'
+> & {
+  field: NumberEditorProps['field'];
+};
+
+function InnerNumberEditor({ disabled, errors, field, setValue, value }: InnerNumberEditorProps) {
+  const previousValue = React.useRef(valueToString(value));
+  const [stringValue, setStringValue] = React.useState(valueToString(value));
+  const range = getRangeFromField(field);
+
+  React.useEffect(() => {
+    previousValue.current = valueToString(value);
+  }, [value]);
+
+  React.useEffect(() => {
+    if (previousValue.current !== String(value) && String(value) !== stringValue) {
+      setStringValue(valueToString(value));
+    }
+  }, [stringValue, value]);
+
+  return (
+    <div data-fifteen data-test-id="number-editor">
+      <TextInput
+        testId="number-editor-input"
+        min={range.min !== undefined ? String(range.min) : ''}
+        max={range.max !== undefined ? String(range.max) : ''}
+        step={field.type === 'Integer' ? '1' : ''}
+        isRequired={field.required}
+        isInvalid={errors.length > 0}
+        isDisabled={disabled}
+        value={stringValue}
+        // React cannot call onChange for certain changes to type="number"
+        // so we use "text" instead. See https://github.com/facebook/react/issues/6556
+        type="text"
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+          const parseResult = parseNumber(e.target.value, field.type);
+          field.setInvalid(!parseResult.isValid);
+
+          if (parseResult.isValid) {
+            setValue(parseResult.value);
+          }
+
+          setStringValue(e.target.value);
+        }}
+      />
+    </div>
+  );
+}
+
 export function NumberEditor(props: NumberEditorProps) {
   const { field } = props;
 
-  const range = getRangeFromField(field);
-
   return (
     <FieldConnector<number> field={field} isInitiallyDisabled={props.isInitiallyDisabled}>
-      {({ value, errors, disabled, setValue }) => {
-        return (
-          <div data-test-id="number-editor">
-            <TextInput
-              testId="number-editor-input"
-              min={range.min !== undefined ? String(range.min) : ''}
-              max={range.max !== undefined ? String(range.max) : ''}
-              step={field.type === 'Integer' ? '1' : ''}
-              type="number"
-              isRequired={field.required}
-              isInvalid={errors.length > 0}
-              isDisabled={disabled}
-              value={value === undefined ? '' : String(value)}
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                const parseResult = parseNumber(e.target.value, field.type);
-                field.setInvalid(!parseResult.isValid);
-                if (parseResult.isValid) {
-                  setValue(parseResult.value);
-                }
-              }}
-            />
-          </div>
-        );
-      }}
+      {({
+        value,
+        errors,
+        disabled,
+        setValue,
+      }: Pick<FieldConnectorChildProps<number>, 'disabled' | 'errors' | 'setValue' | 'value'>) => (
+        <InnerNumberEditor
+          disabled={disabled}
+          errors={errors}
+          field={field}
+          setValue={setValue}
+          value={value}
+        />
+      )}
     </FieldConnector>
   );
 }
