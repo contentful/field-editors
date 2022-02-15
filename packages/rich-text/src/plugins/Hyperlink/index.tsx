9dab957f1b2000c1a033e89e84aff9dadd11c6a8
Luca Bianconi <71259950+IBratWurst@users.noreply.github.com>
2022-02-15 14:31:01 +0100

    fix: restore href check for hyperlinks

--- packages/rich-text/src/plugins/Hyperlink/index.tsx
+++ packages/rich-text/src/plugins/Hyperlink/index.tsx
@@ -182,13 +182,13 @@ export function ToolbarHyperlinkButton(props: ToolbarHyperlinkButtonProps) {
 }
 
 const isAnchor = (element: HTMLElement) =>
-  element.nodeName === 'A' && element.getAttribute('href') !== '#';
+  element.nodeName === 'A' &&
+  !!element.getAttribute('href') &&
+  element.getAttribute('href') !== '#';
 
-const isEntryAnchor = (element: HTMLElement) =>
-  isAnchor(element) && element.getAttribute('data-link-type') === 'Entry';
+const isEntryAnchor = (element: HTMLElement) => element.getAttribute('data-link-type') === 'Entry';
 
-const isAssetAnchor = (element: HTMLElement) =>
-  isAnchor(element) && element.getAttribute('data-link-type') === 'Asset';
+const isAssetAnchor = (element: HTMLElement) => element.getAttribute('data-link-type') === 'Asset';
 
 const buildHyperlinkEventHandler =
   (sdk: FieldExtensionSDK): KeyboardHandler<{}, HotkeyPlugin> =>
