cd2142754e0123520f4412a6b47b0d4805d8e2d2
Luca Bianconi <71259950+IBratWurst@users.noreply.github.com>
2022-02-22 14:18:52 +0100

    fix: pasting blockquote (#1035)

--- packages/rich-text/src/plugins/Quote/createQuotePlugin.ts
+++ packages/rich-text/src/plugins/Quote/createQuotePlugin.ts
@@ -1,4 +1,6 @@
-import { BLOCKS, CONTAINERS } from '@contentful/rich-text-types';
+import { BLOCKS, CONTAINERS, TEXT_CONTAINERS } from '@contentful/rich-text-types';
+import { getAbove } from '@udecode/plate-core';
+import { BaseRange, BaseSelection, Element, Node, Point, Transforms } from 'slate';
 
 import { transformLift, transformUnwrap } from '../../helpers/transformers';
 import { RichTextPlugin } from '../../types';
@@ -39,5 +41,49 @@ export function createQuotePlugin(): RichTextPlugin {
         },
       },
     ],
+    withOverrides: (editor) => {
+      const { insertFragment } = editor;
+
+      editor.insertFragment = (fragment) => {
+        const startingNode = fragment.length && fragment[0];
+        const startsWithBlockquote =
+          Element.isElement(startingNode) && startingNode.type === BLOCKS.QUOTE;
+
+        const containerEntry = getAbove(editor, {
+          match: {
+            type: TEXT_CONTAINERS,
+          },
+        });
+        const containerIsNotEmpty = containerEntry && Node.string(containerEntry[0]) !== '';
+
+        if (startsWithBlockquote && containerIsNotEmpty) {
+          const { selection } = editor;
+          const isContentSelected = (selection: BaseSelection): selection is BaseRange =>
+            !!selection && Point.compare(selection.anchor, selection.focus) !== 0;
+          // if something is selected (highlighted) we replace the selection
+          if (isContentSelected(selection)) {
+            Transforms.delete(editor, { at: selection });
+          }
+
+          // get the cursor entry again, it may be different after deletion
+          const containerEntry = getAbove(editor, {
+            match: {
+              type: TEXT_CONTAINERS,
+            },
+          });
+
+          const containerIsNotEmpty = containerEntry && Node.string(containerEntry[0]) !== '';
+
+          if (containerIsNotEmpty) {
+            Transforms.insertNodes(editor, fragment);
+            return;
+          }
+        }
+
+        insertFragment(fragment);
+      };
+
+      return editor;
+    },
   };
 }
