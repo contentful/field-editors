0f580abec056e03d08be870d23e21bb1d524ef1b
Ahmed T. Ali <ah.tajelsir@gmail.com>
2022-03-17 09:11:58 +0100

    fix(rich-text): moving list items up/down

---
+++ packages/rich-text/src/plugins/List/transforms/moveListItemUp.ts
@@ -0,0 +1,139 @@
+/**
+ * Credit: Modified version of Plate's list plugin
+ * See: https://github.com/udecode/plate/blob/main/packages/nodes/list
+ */
+import {
+  getAbove,
+  getNode,
+  getPluginType,
+  insertNodes,
+  isLastChild,
+  PlateEditor,
+  TElement,
+} from '@udecode/plate-core';
+import { ELEMENT_LI, hasListChild, moveListItemsToList } from '@udecode/plate-list';
+import { Editor, NodeEntry, Path, Transforms } from 'slate';
+
+import { unwrapList } from './unwrapList';
+
+export interface MoveListItemUpOptions {
+  list: NodeEntry<TElement>;
+  listItem: NodeEntry<TElement>;
+}
+
+/**
+ * Move a list item up.
+ */
+export const moveListItemUp = (editor: PlateEditor, { list, listItem }: MoveListItemUpOptions) => {
+  const move = () => {
+    const [listNode, listPath] = list;
+    const [liNode, liPath] = listItem;
+
+    const liParent = getAbove<TElement>(editor, {
+      at: listPath,
+      match: { type: getPluginType(editor, ELEMENT_LI) },
+    });
+    if (!liParent) {
+      let toListPath;
+      try {
+        toListPath = Path.next(listPath);
+      } catch (err) {
+        return;
+      }
+
+      const condA = hasListChild(editor, liNode);
+      const condB = !isLastChild(list, liPath);
+
+      if (condA || condB) {
+        // Insert a new list next to `list`
+        insertNodes<TElement>(
+          editor,
+          {
+            type: listNode.type,
+            children: [],
+            data: {},
+          },
+          { at: toListPath }
+        );
+      }
+
+      if (condA) {
+        const toListNode = getNode<TElement>(editor, toListPath);
+        if (!toListNode) return;
+
+        // Move li sub-lis to the new list
+        moveListItemsToList(editor, {
+          fromListItem: listItem,
+          toList: [toListNode, toListPath],
+        });
+      }
+
+      // If there is siblings li, move them to the new list
+      if (condB) {
+        const toListNode = getNode<TElement>(editor, toListPath);
+        if (!toListNode) return;
+
+        // Move next lis to the new list
+        moveListItemsToList(editor, {
+          fromList: list,
+          fromStartIndex: liPath[liPath.length - 1] + 1,
+          toList: [toListNode, toListPath],
+          deleteFromList: false,
+        });
+      }
+
+      // Finally, unwrap the list
+      unwrapList(editor, { at: liPath.concat(0) });
+
+      return true;
+    }
+    const [, liParentPath] = liParent;
+
+    const toListPath = liPath.concat([1]);
+
+    // If li has next siblings, we need to move them.
+    if (!isLastChild(list, liPath)) {
+      // If li has no sublist, insert one.
+      if (!hasListChild(editor, liNode)) {
+        insertNodes<TElement>(
+          editor,
+          {
+            type: listNode.type,
+            children: [],
+            data: {},
+          },
+          { at: toListPath }
+        );
+      }
+
+      const toListNode = getNode<TElement>(editor, toListPath);
+      if (!toListNode) return;
+
+      // Move next siblings to li sublist.
+      moveListItemsToList(editor, {
+        fromListItem: liParent,
+        toList: [toListNode, toListPath],
+        fromStartIndex: liPath[liPath.length - 1] + 1,
+        deleteFromList: false,
+      });
+    }
+
+    const movedUpLiPath = Path.next(liParentPath);
+
+    // Move li one level up: next to the li parent.
+    Transforms.moveNodes(editor, {
+      at: liPath,
+      to: movedUpLiPath,
+    });
+
+    return true;
+  };
+
+  let moved: boolean | undefined = false;
+
+  Editor.withoutNormalizing(editor, () => {
+    moved = move();
+  });
+
+  return moved;
+};
---
+++ packages/rich-text/src/plugins/List/transforms/moveListItems.ts
@@ -0,0 +1,72 @@
+/**
+ * Credit: Modified version of Plate's list plugin
+ * See: https://github.com/udecode/plate/blob/main/packages/nodes/list
+ */
+import { EditorNodesOptions, getNodes, getPluginType, PlateEditor } from '@udecode/plate-core';
+import { isListNested, ELEMENT_LIC, getListItemEntry } from '@udecode/plate-list';
+import { Editor, Path, PathRef } from 'slate';
+
+import { moveListItemDown } from './moveListItemDown';
+import { moveListItemUp } from './moveListItemUp';
+
+export type MoveListItemsOptions = {
+  increase?: boolean;
+  at?: EditorNodesOptions['at'];
+};
+
+export const moveListItems = (
+  editor: PlateEditor,
+  { increase = true, at = editor.selection ?? undefined }: MoveListItemsOptions = {}
+) => {
+  const _nodes = getNodes(editor, {
+    at,
+    match: {
+      type: getPluginType(editor, ELEMENT_LIC),
+    },
+  });
+
+  // Get the selected lic
+  const lics = Array.from(_nodes);
+
+  if (!lics.length) return;
+
+  const highestLicPaths: Path[] = [];
+  const highestLicPathRefs: PathRef[] = [];
+
+  // Filter out the nested lic, we just need to move the highest ones
+  lics.forEach((lic) => {
+    const licPath = lic[1];
+    const liPath = Path.parent(licPath);
+
+    const isAncestor = highestLicPaths.some((path) => {
+      const highestLiPath = Path.parent(path);
+
+      return Path.isAncestor(highestLiPath, liPath);
+    });
+    if (!isAncestor) {
+      highestLicPaths.push(licPath);
+      highestLicPathRefs.push(Editor.pathRef(editor, licPath));
+    }
+  });
+
+  const licPathRefsToMove = increase ? highestLicPathRefs : highestLicPathRefs.reverse();
+
+  Editor.withoutNormalizing(editor, () => {
+    licPathRefsToMove.forEach((licPathRef) => {
+      const licPath = licPathRef.unref();
+      if (!licPath) return;
+
+      const liEntry = getListItemEntry(editor, { at: licPath });
+
+      if (!liEntry) {
+        return;
+      }
+
+      if (increase) {
+        moveListItemDown(editor, liEntry);
+      } else if (isListNested(editor, liEntry.list[1])) {
+        moveListItemUp(editor, liEntry);
+      }
+    });
+  });
+};
