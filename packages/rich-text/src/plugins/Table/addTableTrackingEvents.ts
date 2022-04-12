7093ecc499e53c8eb58483027574aba05def763e
Ahmed T. Ali <ah.tajelsir@gmail.com>
2022-04-12 10:52:16 +0200

    feat(rich-text): log pasting of invalid table content

--- packages/rich-text/src/plugins/Table/addTableTrackingEvents.ts
+++ packages/rich-text/src/plugins/Table/addTableTrackingEvents.ts
@@ -1,6 +1,8 @@
 import { BLOCKS } from '@contentful/rich-text-types';
+import { Element } from 'slate';
 
-import { CustomElement, RichTextEditor } from '../../types';
+import { CustomElement, RichTextEditor, TextOrCustomElement } from '../../types';
+import { isTable } from './helpers';
 
 function hasTables(nodes: CustomElement[]) {
   return nodes.some(({ type }) => {
@@ -10,11 +12,40 @@ function hasTables(nodes: CustomElement[]) {
 
 const isTableHeaderCell = ({ type }) => type === BLOCKS.TABLE_HEADER_CELL;
 
+const isTableCellOrHeader = (node: TextOrCustomElement): node is CustomElement => {
+  return (
+    Element.isElement(node) &&
+    [BLOCKS.TABLE_HEADER_CELL, BLOCKS.TABLE_CELL].includes(node.type as BLOCKS)
+  );
+};
+
 function hasHeadersOutsideFirstRow(nodes: CustomElement[]) {
   return nodes
     .filter(({ type }) => type === BLOCKS.TABLE)
     .flatMap(({ children }) => children.slice(1) as CustomElement[])
     .some(({ children }) => (children as CustomElement[]).some(isTableHeaderCell));
+}
+
+function trackInvalidTableCellChildren(editor: RichTextEditor, table: CustomElement) {
+  const cells = table.children
+    .flatMap((row) => (row as CustomElement).children ?? [])
+    .filter(isTableCellOrHeader);
+
+  // get invalid direct children
+  const invalidNodeTypes = cells.flatMap((cell) => {
+    // Only paragraphs are allowed inside tables at the moment
+    return cell.children
+      .filter(
+        (node): node is CustomElement => Element.isElement(node) && node.type !== BLOCKS.PARAGRAPH
+      )
+      .map((node) => node.type);
+  });
+
+  for (const nodeType of new Set(invalidNodeTypes)) {
+    editor.tracking?.onViewportAction('invalidTablePaste', {
+      nodeType,
+    });
+  }
 }
 
 export function addTableTrackingEvents(editor: RichTextEditor) {
@@ -38,5 +69,17 @@ export function addTableTrackingEvents(editor: RichTextEditor) {
     } else {
       insertData(data);
     }
+  };
+
+  const { insertFragment } = editor;
+
+  editor.insertFragment = (fragment) => {
+    const tables = (fragment as TextOrCustomElement[]).filter(isTable) as CustomElement[];
+
+    for (const table of tables) {
+      trackInvalidTableCellChildren(editor, table);
+    }
+
+    return insertFragment(fragment);
   };
 }
--- packages/rich-text/src/plugins/Tracking/createTrackingPlugin.ts
+++ packages/rich-text/src/plugins/Tracking/createTrackingPlugin.ts
@@ -18,6 +18,7 @@ export type RichTextTrackingActionName =
   | 'removeTableColumn'
   // Copy & Paste
   | 'paste'
+  | 'invalidTablePaste'
   // Hyperlinks
   | 'cancelCreateHyperlinkDialog'
   | 'cancelEditHyperlinkDialog'
