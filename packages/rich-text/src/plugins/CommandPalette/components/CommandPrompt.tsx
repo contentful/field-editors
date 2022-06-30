5a06c7efe191623eea0f4b7f637ef874d1a671b7
Yves Rijckaert <hi@yvesrijckaert.com>
2022-06-30 12:14:36 +0200

    fix: disable rich text commands in tables

--- packages/rich-text/src/plugins/CommandPalette/components/CommandPrompt.tsx
+++ packages/rich-text/src/plugins/CommandPalette/components/CommandPrompt.tsx
@@ -1,17 +1,22 @@
 import * as React from 'react';
 
+import { BLOCKS } from '@contentful/rich-text-types';
 import { PlateRenderLeafProps } from '@udecode/plate-core';
+import { RichTextEditor } from 'types';
 
+import { isNodeTypeSelected } from '../../../helpers/editor';
 import { trimLeadingSlash } from '../utils/trimLeadingSlash';
 import { CommandList } from './CommandList';
 
 export const CommandPrompt = (props: PlateRenderLeafProps) => {
   const query = trimLeadingSlash(props.text.text);
+  const editor = props.editor as RichTextEditor;
+  const canInsertBlocks = !isNodeTypeSelected(editor, BLOCKS.TABLE);
 
   return (
     <span {...props.attributes}>
       {props.children}
-      <CommandList query={query} editor={props.editor} />
+      {canInsertBlocks && <CommandList query={query} editor={editor} />}
     </span>
   );
 };
