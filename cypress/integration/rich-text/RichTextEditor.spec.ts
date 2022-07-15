fd2acdedcd17b88b561039490b7417edf6d7d15e
Maya Gillilan <maya.gillilan@contentful.com>
2022-07-15 09:51:46 +0200

    test: add tests for rt commands and headings, adjacent text

--- cypress/integration/rich-text/RichTextEditor.spec.ts
+++ cypress/integration/rich-text/RichTextEditor.spec.ts
@@ -430,6 +430,28 @@ describe('Rich Text Editor', { viewportHeight: 2000 }, () => {
           ).to.have.length(1);
         });
       });
+
+      //todo test should not delete adjacent non-command text
+
+      it('should not delete adjacent text', () => {
+        richText.editor.click().type('test/{downarrow}{enter}{enter}');
+        richText.expectSnapshotValue();
+      });
+
+      it('should work inside headings', () => {
+        richText.editor.click().type('Heading 1');
+        richText.toolbar.toggleHeading(BLOCKS.HEADING_1);
+        richText.editor.click().type('/{enter}{enter}');
+
+        //this is used instead of snapshot value because we have randomized entry IDs
+        richText.getValue().should((doc) => {
+          expect(
+            doc.content.filter((node) => {
+              return node.nodeType === BLOCKS.EMBEDDED_ENTRY || node.nodeType === BLOCKS.HEADING_1;
+            })
+          ).to.have.length(2);
+        });
+      });
     });
   });
 
