0d17be95b9f036b6cdbffdc5d4549c9ce2d700d0
bgutsol <bohdan.hutsol@contentful.com>
2022-09-22 10:32:14 +0200

    chore: test minor cahnges

--- packages/date/src/DatepickerInput.spec.tsx
+++ packages/date/src/DatepickerInput.spec.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-global-assign -- need for test purposes */
 import React from 'react';
 
-import { render, configure } from '@testing-library/react';
+import { render, configure, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import timezonedDate from 'timezoned-date';
 
@@ -14,10 +14,10 @@ configure({
 
 let originalDate: typeof Date;
 beforeEach(() => {
-  originalDate = Date;
+  originalDate = global.Date;
 });
 afterEach(() => {
-  Date = originalDate;
+  global.Date = originalDate;
 });
 
 const renderDatepicker = (dateString: string) => {
@@ -32,41 +32,41 @@ const renderDatepicker = (dateString: string) => {
 describe('Date: DatepickerInput', function () {
   describe('renders correct date, when the system UTC offset is +02:00 and the one in the props.value is', () => {
     it('+04:00', async () => {
-      Date = timezonedDate.makeConstructor(120);
-      const { getByTestId } = renderDatepicker('2022-09-22T00:00+04:00');
-      expect(getByTestId('cf-ui-datepicker-input')).toHaveValue('22 Sep 2022');
+      global.Date = timezonedDate.makeConstructor(120);
+      renderDatepicker('2022-09-22T00:00+04:00');
+      expect(screen.getByTestId('cf-ui-datepicker-input')).toHaveValue('22 Sep 2022');
     });
 
     it('+10:00', async () => {
-      Date = timezonedDate.makeConstructor(120);
-      const { getByTestId } = renderDatepicker('2022-09-22T00:00+10:00');
-      expect(getByTestId('cf-ui-datepicker-input')).toHaveValue('22 Sep 2022');
+      global.Date = timezonedDate.makeConstructor(120);
+      renderDatepicker('2022-09-22T00:00+10:00');
+      expect(screen.getByTestId('cf-ui-datepicker-input')).toHaveValue('22 Sep 2022');
     });
 
     it('-08:00', async () => {
-      Date = timezonedDate.makeConstructor(120);
-      const { getByTestId } = renderDatepicker('2022-09-22T00:00-08:00');
-      expect(getByTestId('cf-ui-datepicker-input')).toHaveValue('22 Sep 2022');
+      global.Date = timezonedDate.makeConstructor(120);
+      renderDatepicker('2022-09-22T00:00-08:00');
+      expect(screen.getByTestId('cf-ui-datepicker-input')).toHaveValue('22 Sep 2022');
     });
   });
 
   describe('renders correct date, when the system UTC offset is -06:00 and the one in the props.value is', () => {
     it('+02:00', async () => {
-      Date = timezonedDate.makeConstructor(-360);
-      const { getByTestId } = renderDatepicker('2022-09-22T00:00+02:00');
-      expect(getByTestId('cf-ui-datepicker-input')).toHaveValue('22 Sep 2022');
+      global.Date = timezonedDate.makeConstructor(-360);
+      renderDatepicker('2022-09-22T00:00+02:00');
+      expect(screen.getByTestId('cf-ui-datepicker-input')).toHaveValue('22 Sep 2022');
     });
 
     it('+10:00', async () => {
-      Date = timezonedDate.makeConstructor(-360);
-      const { getByTestId } = renderDatepicker('2022-09-22T00:00+10:00');
-      expect(getByTestId('cf-ui-datepicker-input')).toHaveValue('22 Sep 2022');
+      global.Date = timezonedDate.makeConstructor(-360);
+      renderDatepicker('2022-09-22T00:00+10:00');
+      expect(screen.getByTestId('cf-ui-datepicker-input')).toHaveValue('22 Sep 2022');
     });
 
     it('-08:00', async () => {
-      Date = timezonedDate.makeConstructor(-360);
-      const { getByTestId } = renderDatepicker('2022-09-22T00:00-08:00');
-      expect(getByTestId('cf-ui-datepicker-input')).toHaveValue('22 Sep 2022');
+      global.Date = timezonedDate.makeConstructor(-360);
+      renderDatepicker('2022-09-22T00:00-08:00');
+      expect(screen.getByTestId('cf-ui-datepicker-input')).toHaveValue('22 Sep 2022');
     });
   });
 });
