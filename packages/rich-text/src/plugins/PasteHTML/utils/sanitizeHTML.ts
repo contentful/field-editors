d3364365cf373ff1dffe2fe91b3bbfd412b6c207
Chris Helgert <chrishelgert@users.noreply.github.com>
2023-03-07 10:41:52 +0100

    fix(RTE): remove whitespaces only in tables and lists [TOL-104]

--- packages/rich-text/src/plugins/PasteHTML/utils/sanitizeHTML.ts
+++ packages/rich-text/src/plugins/PasteHTML/utils/sanitizeHTML.ts
@@ -11,7 +11,17 @@ const stripStyleTags = (doc: Document): Document => {
   return doc;
 };
 
-const transformers = [stripStyleTags, sanitizeSheets];
+/**
+ * Remove all <meta /> tags
+ */
+const stripMetaTags = (doc: Document): Document => {
+  doc.querySelectorAll('meta').forEach((el) => el.remove());
+
+  return doc;
+};
+
+// Attention: Order is important
+const transformers = [stripStyleTags, sanitizeSheets, stripMetaTags];
 
 export const sanitizeHTML = (html: string): string => {
   // Parse the HTML string and pipe it through our transformers
@@ -20,7 +30,26 @@ export const sanitizeHTML = (html: string): string => {
     new DOMParser().parseFromString(html, 'text/html')
   );
 
-  return doc.body.innerHTML
-    .replace(/>\s+</g, '><') // Remove whitespace between tags
-    .replace(/(.*)<div.*>(<table.*<\/table>)<\/div>(.*)/g, '$1$2$3'); // remove div containers from tables
+  let previous: string;
+  do {
+    // save previous first before doing modifications
+    previous = doc.body.innerHTML;
+    // Update the body with the cleaned up content
+    doc.body.innerHTML = doc.body.innerHTML
+      // remove div container from tables
+      // capture groups explained:
+      // 1. and 3. every content/linebreaks before and after the div container
+      // 2. the table inside the container, including content and linebreaks
+      // The div container including attributes and possible linebreaks inside wil be removed
+      .replace(/(.*\s)?<div.*>\s*(<table(?:.|\s)*<\/table>)\s*<\/div>(.*\s)?/g, '$1$2$3')
+      // remove whitespaces between some tags, as this can lead to unwanted behaviour:
+      // - table -> empty table cells
+      // - list -> leading whitespaces
+      .replace(
+        /<(\/)?(table|thead|tbody|tr|td|th|caption|col|colgroup|ol|ul|li)(.*)>\s+</g,
+        '<$1$2$3><'
+      );
+  } while (doc.body.innerHTML !== previous);
+
+  return doc.body.innerHTML;
 };
