f841af781ba3cd9aced02a828de6918af4ef975e
Chris Helgert <chrishelgert@users.noreply.github.com>
2023-03-20 17:46:04 +0100

    chore(RTE): increase performance for pasting in text

--- packages/rich-text/src/plugins/PasteHTML/utils/sanitizeHTML.ts
+++ packages/rich-text/src/plugins/PasteHTML/utils/sanitizeHTML.ts
@@ -21,6 +21,8 @@ const stripMetaTags = (doc: Document): Document => {
   return doc;
 };
 
+type Replacer = (innerHTML: string) => string;
+
 // Attention: Order is important
 const transformers = [stripStyleTags, sanitizeSheets, stripMetaTags, sanitizeAnchors];
 
@@ -31,32 +33,50 @@ export const sanitizeHTML = (html: string): string => {
     new DOMParser().parseFromString(html, 'text/html')
   );
 
+  const replacers: Replacer[] = [
+    // remove whitespaces between some tags, as this can lead to unwanted behaviour:
+    // - table -> empty table cells
+    // - list -> leading whitespaces
+    (innerHtml) =>
+      innerHtml.replace(
+        /<(\/)?(table|thead|tbody|tr|td|th|caption|col|colgroup|ol|ul|li)(.*)>\s+<(\/)?(table|thead|tbody|tr|td|th|caption|col|colgroup|ol|ul|li)/g,
+        '<$1$2$3><$4$5'
+      ),
+    // remove empty elements before the ending block element tag
+    (innerHtml) =>
+      innerHtml.replace(
+        /(?:<[^>^/]*>)\s*(?:<\/[^>]*>)<\/(div|p|table|thead|tbody|tr|td|th|caption|col|colgroup|ol|ul|li)/g,
+        '</$1'
+      ),
+    // remove whitespaces before the ending block element tag
+    (innerHTML) =>
+      innerHTML.replace(
+        /\s*<\/(div|p|table|thead|tbody|tr|td|th|caption|col|colgroup|ol|ul|li)/g,
+        '</$1'
+      ),
+  ];
+
+  // Call this replacer only if there are tables inside, as it can have a larger performance impact
+  if (doc.querySelector('table')) {
+    // remove div container from tables
+    // capture groups explained:
+    // 1. and 3. every content/linebreaks before and after the div container
+    // 2. the table inside the container, including content and linebreaks
+    // The div container including attributes and possible linebreaks inside wil be removed
+    replacers.unshift((innerHtml) =>
+      innerHtml.replace(/(.*\s)?<div.*>\s*(<table(?:.|\s)*<\/table>)\s*<\/div>(.*\s)?/g, '$1$2$3')
+    );
+  }
+
   let previous: string;
   do {
     // save previous first before doing modifications
     previous = doc.body.innerHTML;
-    // Update the body with the cleaned up content
-    doc.body.innerHTML = doc.body.innerHTML
-      // remove div container from tables
-      // capture groups explained:
-      // 1. and 3. every content/linebreaks before and after the div container
-      // 2. the table inside the container, including content and linebreaks
-      // The div container including attributes and possible linebreaks inside wil be removed
-      .replace(/(.*\s)?<div.*>\s*(<table(?:.|\s)*<\/table>)\s*<\/div>(.*\s)?/g, '$1$2$3')
-      // remove whitespaces between some tags, as this can lead to unwanted behaviour:
-      // - table -> empty table cells
-      // - list -> leading whitespaces
-      .replace(
-        /<(\/)?(table|thead|tbody|tr|td|th|caption|col|colgroup|ol|ul|li)(.*)>\s+<(\/)?(table|thead|tbody|tr|td|th|caption|col|colgroup|ol|ul|li)/g,
-        '<$1$2$3><$4$5'
-      )
-      // remove empty elements before the ending block element tag
-      .replace(
-        /(?:<[^>^/]*>)\s*(?:<\/[^>]*>)<\/(div|p|table|thead|tbody|tr|td|th|caption|col|colgroup|ol|ul|li)/g,
-        '</$1'
-      )
-      // remove whitespaces before the ending block element tag
-      .replace(/\s*<\/(div|p|table|thead|tbody|tr|td|th|caption|col|colgroup|ol|ul|li)/g, '</$1');
+
+    doc.body.innerHTML = replacers.reduce(
+      (innerHTML, replacer) => replacer(innerHTML),
+      doc.body.innerHTML
+    );
   } while (doc.body.innerHTML !== previous);
 
   return doc.body.innerHTML;
