23e036e6024a54c44574be8e381409e9867f90cb
Kudakwashe Mupeni <terrencemupeni@gmail.com>
2023-04-18 15:14:45 +0200

    chore: cleanup cypress upgrade [TOL-1036]

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from 'cypress';
-
-// @ts-expect-error -- no types
-import webpackConfig from './webpack.config.js';
+import webpack from 'webpack';
 
 const task = {
   log(message: string) {
@@ -42,7 +40,44 @@ export default defineConfig({
     devServer: {
       framework: 'react',
       bundler: 'webpack',
-      webpackConfig,
+      webpackConfig: {
+        resolve: {
+          extensions: ['.ts', '.tsx', '.js', '.jsx', '.mjs'],
+        },
+        // needed to prevent ReferenceErrors
+        // cf. https://github.com/webpack/webpack/issues/6693#issuecomment-745688108
+        output: {
+          hotUpdateChunkFilename: '[id].[fullhash].hot-update.js',
+          hotUpdateMainFilename: '[runtime].[fullhash].hot-update.json',
+        },
+        performance: false,
+        module: {
+          rules: [
+            {
+              test: /\.tsx?$/,
+              exclude: [/node_modules/],
+              use: [
+                {
+                  loader: 'ts-loader',
+                  options: {
+                    configFile: 'cypress/tsconfig.json',
+                    transpileOnly: true,
+                  },
+                },
+              ],
+            },
+            {
+              test: /\.mjs$/,
+              type: 'javascript/auto',
+            },
+          ],
+        },
+        plugins: [
+          new webpack.ProvidePlugin({
+            process: 'process/browser.js',
+          }),
+        ],
+      },
     },
     specPattern: 'cypress/component/**/*.spec.{js,ts,jsx,tsx}',
     excludeSpecPattern: ['**/__snapshots__/*', '**/__image_snapshots__/*'],
