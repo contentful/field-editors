8c805dff9c03578cd923fda3c6b2bd7f9d7d20d5
Kudakwashe Mupeni <terrencemupeni@gmail.com>
2023-04-19 16:20:11 +0200

    fix: update cypress & refactor webpack [TOL-1036]

--- cypress/component/mount.tsx
+++ cypress/component/mount.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 
 import { GlobalStyles } from '@contentful/f36-core';
 import tokens from '@contentful/f36-tokens';
-import { mount as cyMount, MountReturn } from '@cypress/react';
-import { MountOptions } from '@cypress/react';
+import { mount as cyMount, MountReturn, MountOptions } from '@cypress/react';
 import { Global } from '@emotion/core';
 
 function TestStyles() {
