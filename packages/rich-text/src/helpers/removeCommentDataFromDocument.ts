fbf061f569fc4455fcbc970f340dca599fca08a3
mdmjg <mdj308@nyu.edu>
2023-06-01 13:37:38 +0200

    fix: also removes marks

--- packages/rich-text/src/helpers/removeCommentDataFromDocument.ts
+++ packages/rich-text/src/helpers/removeCommentDataFromDocument.ts
@@ -1,9 +1,16 @@
+import { INLINE_COMMENT_HIGHLIGHT } from 'plugins/Marks';
+
 export const removeCommentDataFromDocument = (document: any): any => {
   if (typeof document !== 'object' || document === null) {
     return document;
   }
-  if ('comment' in document) {
-    return {};
+  if ('comment' in document.data) {
+    document.data = {};
+    const indexOfMark = document.marks.indexOf({ type: INLINE_COMMENT_HIGHLIGHT });
+    if (indexOfMark > -1) {
+      document.marks.splice(indexOfMark, 1);
+    }
+    return document;
   }
 
   if (Array.isArray(document)) {
