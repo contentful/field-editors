8cd5f7c9bd3f1bc0fa246c4d1c006be3c9ded15f
Cyberxon <marouenbs93@gmail.com>
2023-08-10 10:33:44 +0200

    feat: adding environment support in the WrappedEntryCard

--- packages/reference/src/components/SpaceName/SpaceName.tsx
+++ packages/reference/src/components/SpaceName/SpaceName.tsx
@@ -5,19 +5,20 @@ import { FolderOpenTrimmedIcon } from '@contentful/f36-icons';
 import tokens from '@contentful/f36-tokens';
 import { css } from 'emotion';
 
-interface SpaceNameProps {
+interface SourceProps {
   spaceName: string;
+  environmentName?: string;
 }
 
 const styles = {
   spaceIcon: css({
     flexShrink: 0,
-    fill: tokens.purple600,
+    fill: tokens.gray600,
   }),
   spaceName: css({
-    color: tokens.gray700,
+    color: tokens.gray600,
     fontSize: tokens.fontSizeS,
-    fontWeight: tokens.fontWeightDemiBold,
+    fontWeight: tokens.fontWeightMedium,
     maxWidth: '80px',
     textOverflow: 'ellipsis',
     overflow: 'hidden',
@@ -25,9 +26,12 @@ const styles = {
   }),
 };
 
-export function SpaceName(props: SpaceNameProps) {
+export function SpaceName(props: SourceProps) {
+  let content = `Space: ${props.spaceName}`;
+  if (props.environmentName) content += ` (Env.: ${props.environmentName})`;
+
   return (
-    <Tooltip placement="top" as="div" content={`Space: ${props.spaceName}`}>
+    <Tooltip placement="top" as="div" content={content}>
       <Flex alignItems="center" gap="spacingXs" marginRight="spacingS">
         <FolderOpenTrimmedIcon className={styles.spaceIcon} size="tiny" aria-label="Source space" />
         <Text className={styles.spaceName}>{props.spaceName}</Text>
--- packages/reference/src/resources/Cards/ResourceCard.spec.tsx
+++ packages/reference/src/resources/Cards/ResourceCard.spec.tsx
@@ -6,6 +6,7 @@ import { createFakeCMAAdapter } from '@contentful/field-editor-test-utils';
 import { configure, render, waitFor } from '@testing-library/react';
 
 import publishedCT from '../../__fixtures__/content-type/published_content_type.json';
+import publishedEntryNonMasterEnvironment from '../../__fixtures__/entry/published_entry_non_master.json';
 import publishedEntry from '../../__fixtures__/entry/published_entry.json';
 import space from '../../__fixtures__/space/indifferent_space.json';
 import { EntityProvider } from '../../common/EntityStore';
@@ -19,7 +20,12 @@ jest.mock('react-intersection-observer', () => ({
   useInView: jest.fn().mockReturnValue({}),
 }));
 
+// explicit master
 const resolvableEntryUrn = 'crn:contentful:::content:spaces/space-id/entries/linked-entry-urn';
+const resolvableEntryUrnWithImplicitMaster =
+  'crn:contentful:::content:spaces/space-id/environments/master/entries/linked-entry-urn';
+const resolvableEntryUrnWithAnotherEnvironment =
+  'crn:contentful:::content:spaces/space-id/environments/my-test-environment/entries/linked-entry-urn';
 const unknownEntryUrn = 'crn:contentful:::content:spaces/space-id/entries/unknown-entry-urn';
 
 const sdk: any = {
@@ -29,9 +35,20 @@ const sdk: any = {
   cmaAdapter: createFakeCMAAdapter({
     ContentType: { get: jest.fn().mockReturnValue(publishedCT) },
     Entry: {
-      get: jest.fn().mockImplementation(({ entryId }) => {
-        if (entryId === 'linked-entry-urn') {
+      get: jest.fn().mockImplementation(({ spaceId, environmentId, entryId }) => {
+        if (
+          spaceId === 'space-id' &&
+          environmentId === 'master' &&
+          entryId === 'linked-entry-urn'
+        ) {
           return Promise.resolve(publishedEntry);
+        }
+        if (
+          spaceId === 'space-id' &&
+          environmentId === 'my-test-environment' &&
+          entryId === 'linked-entry-urn'
+        ) {
+          return Promise.resolve(publishedEntryNonMasterEnvironment);
         }
         return Promise.reject(new Error());
       }),
@@ -67,11 +84,31 @@ function renderResourceCard({ linkType = 'Contentful:Entry', entryUrn = resolvab
 }
 
 describe('ResourceCard', () => {
-  it('renders entry card', async () => {
+  it('renders entry card with implicit master crn', async () => {
     const { getByTestId, getByText } = renderResourceCard();
 
     await waitFor(() => expect(getByTestId('cf-ui-entry-card')).toBeDefined());
     expect(getByText(publishedEntry.fields.exField['en-US'])).toBeDefined();
+    expect(getByText(space.name)).toBeDefined();
+  });
+
+  it('renders entry card with explicit master crn', async () => {
+    const { getByTestId, getByText } = renderResourceCard({
+      entryUrn: resolvableEntryUrnWithImplicitMaster,
+    });
+
+    await waitFor(() => expect(getByTestId('cf-ui-entry-card')).toBeDefined());
+    expect(getByText(publishedEntry.fields.exField['en-US'])).toBeDefined();
+    expect(getByText(space.name)).toBeDefined();
+  });
+
+  it('renders entry card with a non master environment', async () => {
+    const { getByTestId, getByText } = renderResourceCard({
+      entryUrn: resolvableEntryUrnWithAnotherEnvironment,
+    });
+
+    await waitFor(() => expect(getByTestId('cf-ui-entry-card')).toBeDefined());
+    expect(getByText(publishedEntryNonMasterEnvironment.fields.exField['en-US'])).toBeDefined();
     expect(getByText(space.name)).toBeDefined();
   });
 
@@ -93,6 +130,12 @@ describe('ResourceCard', () => {
 
   it('renders missing entity card when unknown error is returned', async () => {
     const { getByTestId } = renderResourceCard({ entryUrn: unknownEntryUrn });
+
+    await waitFor(() => expect(getByTestId('cf-ui-missing-entry-card')).toBeDefined());
+  });
+
+  it('renders missing entity card when crn is invalid', async () => {
+    const { getByTestId } = renderResourceCard({ entryUrn: '' });
 
     await waitFor(() => expect(getByTestId('cf-ui-missing-entry-card')).toBeDefined());
   });
