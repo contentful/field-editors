60ea9a8723be01cd2b3f0eda5d5c4c73e339a2c5
Ahmed T. Ali <12673605+z0al@users.noreply.github.com>
2023-08-24 13:04:25 +0200

    feat(reference): enable sharing query cache between editors (#1468)

--- packages/reference/src/index.tsx
+++ packages/reference/src/index.tsx
@@ -23,6 +23,7 @@ export type {
 } from './common/customCardTypes';
 export { SortableLinkList } from './common/SortableLinkList';
 export { EntityProvider, useEntityLoader, useEntity, useResource } from './common/EntityStore';
+export { SharedQueryClientProvider as EntityCacheProvider } from './common/queryClient';
 export type { ResourceInfo } from './common/EntityStore';
 export { SingleResourceReferenceEditor, MultipleResourceReferenceEditor } from './resources';
 export * from './types';
