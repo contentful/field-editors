8d68de27c102f9965cdc92f8150295e7aa5b9722
Aodhagan Murphy <aodhagan.murphy@contentful.com>
2023-08-31 15:56:10 +0100

    feat: fixing jest config []
    feat: fixing jest config []
    
    feat: fixing jest config []
    
    feat: fixing jest config []
    
    feat: fixing jest config []
    
    feat: fixing jest config []
    
    feat: fixing jest config []
    
    feat: fixing jest config []
    
    feat: fixing jest config []
    
    feat: fixing jest config []

--- packages/markdown/src/index.tsx
+++ packages/markdown/src/index.tsx
@@ -1,8 +1,6 @@
 import './codemirrorImports';
 
 export { MarkdownEditorConnected as MarkdownEditor } from './MarkdownEditor';
+export { default as MarkdownPreview } from './components/MarkdownPreview';
 export { openMarkdownDialog } from './dialogs/openMarkdownDialog';
 export { renderMarkdownDialog } from './dialogs/renderMarkdownDialog';
-
-import MarkdownPreview from './components/MarkdownPreview';
-export { MarkdownPreview };
