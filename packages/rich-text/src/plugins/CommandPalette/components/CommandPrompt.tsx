fc158c918af2433bba32483a30cb7c26ef771cc2
Yves Rijckaert <hi@yvesrijckaert.com>
2023-09-26 20:33:52 +0200

    feat: set color when rich text command active

--- packages/rich-text/src/plugins/CommandPalette/components/CommandPrompt.tsx
+++ packages/rich-text/src/plugins/CommandPalette/components/CommandPrompt.tsx
@@ -1,8 +1,17 @@
 import * as React from 'react';
 
+import tokens from '@contentful/f36-tokens';
+import { css } from 'emotion';
+
 import { PlateEditor, RenderLeafProps } from '../../../internal/types';
 import { trimLeadingSlash } from '../utils/trimLeadingSlash';
 import { CommandList } from './CommandList';
+
+const styles = {
+  commandPrompt: css({
+    color: tokens.blue400,
+  }),
+};
 
 export const CommandPrompt = (props: RenderLeafProps & { editor: PlateEditor }) => {
   const query = React.useMemo(() => trimLeadingSlash(props.text.text), [props.text.text]);
@@ -11,6 +20,7 @@ export const CommandPrompt = (props: RenderLeafProps & { editor: PlateEditor }) 
 
   return (
     <span
+      className={styles.commandPrompt}
       ref={(e) => {
         setTextElement(e as HTMLSpanElement);
       }}
