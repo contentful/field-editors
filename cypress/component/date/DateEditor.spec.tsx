83d59e31b280df04231a71fe5dca59f71a0d6dfd
Maya Gillilan <101652314+MayaGillilanContentful@users.noreply.github.com>
2023-10-04 15:41:41 +0200

    test: migrate date editor to component tests

--- cypress/component/date/DateEditor.spec.tsx
+++ cypress/component/date/DateEditor.spec.tsx
@@ -1,4 +1,10 @@
-import { getIframe } from '../fixtures/utils';
+import * as React from 'react';
+
+import { createFakeFieldAPI } from '@contentful/field-editor-test-utils';
+
+import { DateEditor } from '../../../packages/date/src/DateEditor';
+import { DateTimeFormat, TimeFormat } from '../../../packages/date/src/types';
+import { mount } from '../mount';
 
 const MONTHS = {
   0: 'Jan',
@@ -15,93 +21,63 @@ const MONTHS = {
   11: 'Dec',
 };
 
+const now = new Date();
+
+const selectors = {
+  getDateInput: () => cy.findByTestId('cf-ui-datepicker-input'),
+  getCalendarTrigger: () => cy.findByTestId('cf-ui-datepicker-button'),
+  getTimeInput: () => cy.findByTestId('time-input'),
+  getTimezoneInput: () => cy.findByTestId('timezone-input'),
+  getClearBtn: () => cy.findByTestId('date-clear'),
+  getCalendar: () => cy.get('.rdp'),
+  getCalendarMonth: () => cy.get('.rdp select[aria-label="Month: "]'),
+  getCalendarYear: () => cy.get('.rdp select[aria-label="Year: "]'),
+  getCalendarTodayDate: () => cy.get('.rdp .rdp-day_today'),
+  getCalendarSelectedDate: () => cy.get('.rdp .rdp-day_selected'),
+};
+
+const getToday = () => {
+  const month = now.getMonth();
+  const year = now.getFullYear();
+  const date = now.getDate();
+  return {
+    month,
+    year,
+    date,
+  };
+};
+
+const getTodayShortString = () => {
+  const date = `${getToday().date}`.padStart(2, '0');
+  const month = MONTHS[getToday().month];
+  const year = getToday().year;
+
+  return `${date} ${month} ${year}`;
+};
+
+const getTodayKebabString = () => {
+  const date = `${getToday().date}`.padStart(2, '0');
+  const month = `${getToday().month + 1}`.padStart(2, '0');
+
+  return `${getToday().year}-${month}-${date}`;
+};
+
+const getTimezoneOffsetString = () => {
+  const currentDate = new Date();
+  const offsetInMinutes = currentDate.getTimezoneOffset();
+  const offsetHours = Math.floor(Math.abs(offsetInMinutes) / 60);
+  const offsetMinutes = Math.abs(offsetInMinutes) % 60;
+  const offsetSign = offsetInMinutes <= 0 ? '+' : '-';
+
+  return `${offsetSign}${offsetHours.toString().padStart(2, '0')}:${offsetMinutes
+    .toString()
+    .padStart(2, '0')}`;
+};
 describe('Date Editor', () => {
-  const now = new Date();
-
-  const selectors = {
-    getDateInput: () => {
-      return getIframe().findByTestId('cf-ui-datepicker-input');
-    },
-    getCalendarTrigger: () => {
-      return getIframe().findByTestId('cf-ui-datepicker-button');
-    },
-    getTimeInput: () => {
-      return getIframe().findByTestId('time-input');
-    },
-    getTimezoneInput: () => {
-      return getIframe().findByTestId('timezone-input');
-    },
-    getClearBtn: () => {
-      return getIframe().findByTestId('date-clear');
-    },
-    getCalendar: () => {
-      return getIframe().find('.rdp');
-    },
-    getCalendarMonth: () => {
-      return getIframe().find('.rdp select[aria-label="Month: "]');
-    },
-    getCalendarYear: () => {
-      return getIframe().find('.rdp select[aria-label="Year: "]');
-    },
-    getCalendarTodayDate: () => {
-      return getIframe().find('.rdp .rdp-day_today');
-    },
-    getCalendarSelectedDate: () => {
-      return getIframe().find('.rdp .rdp-day_selected');
-    },
-  };
-
-  const getToday = () => {
-    const month = now.getMonth();
-    const year = now.getFullYear();
-    const date = now.getDate();
-    return {
-      month,
-      year,
-      date,
-    };
-  };
-
-  const getTodayShortString = () => {
-    const date = `${getToday().date}`.padStart(2, '0');
-    const month = MONTHS[getToday().month];
-    const year = getToday().year;
-
-    return `${date} ${month} ${year}`;
-  };
-
-  const getTodayKebabString = () => {
-    const date = `${getToday().date}`.padStart(2, '0');
-    const month = `${getToday().month + 1}`.padStart(2, '0');
-
-    return `${getToday().year}-${month}-${date}`;
-  };
-
-  const getTimezoneOffsetString = () => {
-    const currentDate = new Date();
-    const offsetInMinutes = currentDate.getTimezoneOffset();
-    const offsetHours = Math.floor(Math.abs(offsetInMinutes) / 60);
-    const offsetMinutes = Math.abs(offsetInMinutes) % 60;
-    const offsetSign = offsetInMinutes <= 0 ? '+' : '-';
-
-    return `${offsetSign}${offsetHours.toString().padStart(2, '0')}:${offsetMinutes
-      .toString()
-      .padStart(2, '0')}`;
-  };
-
-  const openPage = () => {
-    cy.visit('/?path=/story/editors-date--default');
-  };
-
-  beforeEach(() => {
-    cy.visit('/?path=/story/editors-date--default');
-  });
-
   describe('disabled state', () => {
     it('all fields should be disabled', () => {
-      cy.setInitialDisabled(true);
-      openPage();
-      cy.wait(500);
+      const [fieldSdk] = createFakeFieldAPI();
+      mount(<DateEditor field={fieldSdk} isInitiallyDisabled={true} />);
 
       selectors.getDateInput().should('be.disabled');
       selectors.getTimeInput().should('be.disabled');
@@ -112,8 +88,8 @@ describe('Date Editor', () => {
 
   describe('default configuration', () => {
     it('should read initial value', () => {
-      openPage();
-      cy.setInitialValue('2018-01-03T05:53+03:00');
+      const [fieldSdk] = createFakeFieldAPI(undefined, '2018-01-03T05:53+03:00');
+      mount(<DateEditor field={fieldSdk} isInitiallyDisabled={false} />);
 
       selectors.getDateInput().should('have.value', '03 Jan 2018');
       selectors.getTimeInput().should('have.value', '05:53');
@@ -121,7 +97,9 @@ describe('Date Editor', () => {
     });
 
     it('should render date, time (24 format) and timezone inputs by default', () => {
-      openPage();
+      const [fieldSdk] = createFakeFieldAPI();
+      mount(<DateEditor field={fieldSdk} isInitiallyDisabled={false} />);
+
       selectors.getDateInput().should('be.visible').should('have.value', '');
       selectors
         .getTimeInput()
@@ -130,7 +108,6 @@ describe('Date Editor', () => {
         .should('have.attr', 'placeholder', '00:00')
         .should('have.value', '00:00');
 
-      // TODO find a better way
       selectors
         .getTimezoneInput()
         .should('be.visible')
@@ -138,7 +115,9 @@ describe('Date Editor', () => {
     });
 
     it('calendar should show current year, month and date', () => {
-      openPage();
+      const [fieldSdk] = createFakeFieldAPI();
+      mount(<DateEditor field={fieldSdk} isInitiallyDisabled={false} />);
+
       const { year, month, date } = getToday();
 
       selectors.getCalendarTrigger().click();
@@ -151,46 +130,46 @@ describe('Date Editor', () => {
     });
 
     it('correct actions are called when user interacts with editor', () => {
-      openPage();
+      const [fieldSdk] = createFakeFieldAPI();
+      mount(<DateEditor field={fieldSdk} isInitiallyDisabled={false} />);
+
+      cy.spy(fieldSdk, 'setValue').as('setValue');
+      cy.spy(fieldSdk, 'removeValue').as('removeValue');
       selectors.getTimezoneInput().select('+08:00').should('have.value', '+08:00');
 
       selectors.getCalendarTrigger().click();
       selectors.getCalendarTodayDate().click();
 
       selectors.getDateInput().should('have.value', getTodayShortString());
+      cy.get('@setValue').should('have.been.calledWith', getTodayKebabString() + 'T00:00+08:00');
 
       selectors.getTimeInput().focus().type('15:00').should('have.value', '15:00');
+      selectors.getDateInput().click();
+      cy.get('@setValue').should('have.been.calledWith', getTodayKebabString() + 'T15:00+08:00');
 
       selectors.getClearBtn().click();
+      cy.get('@removeValue').should('have.been.called');
 
       selectors.getDateInput().should('have.value', '');
       selectors.getTimeInput().should('have.value', '00:00');
 
       selectors.getTimezoneInput().should('have.value', getTimezoneOffsetString());
       selectors.getClearBtn().should('not.exist');
-
-      cy.editorEvents().should('deep.equal', [
-        { id: 6, type: 'onValueChanged', value: undefined },
-        { id: 5, type: 'removeValue', value: undefined },
-        { id: 4, type: 'onValueChanged', value: getTodayKebabString() + 'T15:00+08:00' },
-        { id: 3, type: 'setValue', value: getTodayKebabString() + 'T15:00+08:00' },
-        { id: 2, type: 'onValueChanged', value: getTodayKebabString() + 'T00:00+08:00' },
-        { id: 1, type: 'setValue', value: getTodayKebabString() + 'T00:00+08:00' },
-      ]);
     });
 
     it('should reset field state on external change', () => {
-      cy.setInitialValue('1990-01-03T22:53+03:00');
-
-      openPage();
+      const [fieldSdk] = createFakeFieldAPI(undefined, '1990-01-03T22:53+03:00');
+      mount(<DateEditor field={fieldSdk} isInitiallyDisabled={false} />);
 
       selectors.getDateInput().should('have.value', '03 Jan 1990');
       selectors.getTimeInput().should('have.value', '22:53');
-      selectors.getTimezoneInput().should('have.value', '+03:00');
-
-      cy.setValueExternal('1992-01-03T21:40+05:00');
-
-      cy.wait(500);
+      selectors
+        .getTimezoneInput()
+        .should('have.value', '+03:00')
+        .then(() => {
+          //TODO: A bit awkward to have this in a .then(), any other way to make it wait for prev tests?
+          fieldSdk.setValue('1992-01-03T21:40+05:00');
+        });
 
       selectors.getDateInput().should('have.value', '03 Jan 1992');
       selectors.getTimeInput().should('have.value', '21:40');
@@ -198,7 +177,9 @@ describe('Date Editor', () => {
     });
 
     it('should parse values in time input', () => {
-      openPage();
+      const [fieldSdk] = createFakeFieldAPI();
+      mount(<DateEditor field={fieldSdk} isInitiallyDisabled={false} />);
+
       selectors.getTimeInput().should('have.value', '00:00');
 
       const pairs = [
@@ -219,8 +200,8 @@ describe('Date Editor', () => {
     });
 
     it('should show the correct date regardless of the time and timezone #1', () => {
-      cy.setInitialValue('2022-11-01T00:00+02:00');
-      openPage();
+      const [fieldSdk] = createFakeFieldAPI(undefined, '2022-11-01T00:00+02:00');
+      mount(<DateEditor field={fieldSdk} isInitiallyDisabled={false} />);
 
       selectors.getDateInput().should('have.value', '01 Nov 2022');
       selectors.getTimeInput().should('have.value', '00:00');
@@ -235,8 +216,8 @@ describe('Date Editor', () => {
     });
 
     it('should show the correct date regardless of the time and timezone #2', () => {
-      cy.setInitialValue('2022-11-01T00:00+12:00');
-      openPage();
+      const [fieldSdk] = createFakeFieldAPI(undefined, '2022-11-01T00:00+12:00');
+      mount(<DateEditor field={fieldSdk} isInitiallyDisabled={false} />);
 
       selectors.getDateInput().should('have.value', '01 Nov 2022');
       selectors.getTimeInput().should('have.value', '00:00');
@@ -252,16 +233,17 @@ describe('Date Editor', () => {
   });
 
   describe('without timezone and with AM/PM', () => {
-    beforeEach(() => {
-      cy.setInstanceParams({
-        format: 'time',
-        ampm: '12',
-      });
-    });
+    const parameters = {
+      installation: {},
+      instance: {
+        format: 'time' as DateTimeFormat,
+        ampm: '12' as TimeFormat,
+      },
+    };
 
     it('should read initial value', () => {
-      cy.setInitialValue('1990-01-03T22:53+03:00');
-      openPage();
+      const [fieldSdk] = createFakeFieldAPI(undefined, '1990-01-03T22:53+03:00');
+      mount(<DateEditor field={fieldSdk} isInitiallyDisabled={false} parameters={parameters} />);
 
       selectors.getDateInput().should('have.value', '03 Jan 1990');
       selectors.getTimeInput().should('have.value', '10:53 PM');
@@ -276,7 +258,9 @@ describe('Date Editor', () => {
     });
 
     it('should parse values in time input', () => {
-      openPage();
+      const [fieldSdk] = createFakeFieldAPI();
+      mount(<DateEditor field={fieldSdk} isInitiallyDisabled={false} parameters={parameters} />);
+
       selectors.getTimeInput().should('have.value', '12:00 AM');
 
       const pairs = [
@@ -297,43 +281,42 @@ describe('Date Editor', () => {
     });
 
     it('correct actions are called when user interacts with editor', () => {
-      openPage();
+      const [fieldSdk] = createFakeFieldAPI();
+      mount(<DateEditor field={fieldSdk} isInitiallyDisabled={false} parameters={parameters} />);
+
+      cy.spy(fieldSdk, 'setValue').as('setValue');
+      cy.spy(fieldSdk, 'removeValue').as('removeValue');
 
       selectors.getCalendarTrigger().click();
       selectors.getCalendarTodayDate().click();
       selectors.getDateInput().should('have.value', getTodayShortString());
+      cy.get('@setValue').should('have.been.calledWith', getTodayKebabString() + 'T00:00');
 
       selectors.getTimeInput().focus().type('3:00 PM');
       selectors.getDateInput().click();
       selectors.getTimeInput().should('have.value', '03:00 PM');
+      cy.get('@setValue').should('have.been.calledWith', getTodayKebabString() + 'T15:00');
 
       selectors.getClearBtn().click();
+      cy.get('@removeValue').should('have.been.called');
 
       selectors.getDateInput().should('have.value', '');
       selectors.getTimeInput().should('have.value', '12:00 AM');
       selectors.getClearBtn().should('not.exist');
-
-      cy.editorEvents().should('deep.equal', [
-        { id: 6, type: 'onValueChanged', value: undefined },
-        { id: 5, type: 'removeValue', value: undefined },
-        { id: 4, type: 'onValueChanged', value: getTodayKebabString() + 'T15:00' },
-        { id: 3, type: 'setValue', value: getTodayKebabString() + 'T15:00' },
-        { id: 2, type: 'onValueChanged', value: getTodayKebabString() + 'T00:00' },
-        { id: 1, type: 'setValue', value: getTodayKebabString() + 'T00:00' },
-      ]);
     });
 
     it('should reset field state on external change', () => {
-      cy.setInitialValue('1990-01-03T22:53');
-
-      openPage();
+      const [fieldSdk] = createFakeFieldAPI(undefined, '1990-01-03T22:53');
+      mount(<DateEditor field={fieldSdk} isInitiallyDisabled={false} parameters={parameters} />);
 
       selectors.getDateInput().should('have.value', '03 Jan 1990');
-      selectors.getTimeInput().should('have.value', '10:53 PM');
-
-      cy.setValueExternal('1992-01-03T21:40');
-
-      cy.wait(500);
+      selectors
+        .getTimeInput()
+        .should('have.value', '10:53 PM')
+        .then(() => {
+          //TODO: same as before
+          fieldSdk.setValue('1992-01-03T21:40');
+        });
 
       selectors.getDateInput().should('have.value', '03 Jan 1992');
       selectors.getTimeInput().should('have.value', '09:40 PM');
@@ -341,15 +324,15 @@ describe('Date Editor', () => {
   });
 
   describe('without timezone and time', () => {
-    beforeEach(() => {
-      cy.setInstanceParams({
-        format: 'dateonly',
-      });
-    });
-
+    const parameters = {
+      installation: {},
+      instance: {
+        format: 'dateonly' as DateTimeFormat,
+      },
+    };
     it('should read initial value', () => {
-      cy.setInitialValue('1990-01-03T22:53');
-      openPage();
+      const [fieldSdk] = createFakeFieldAPI(undefined, '1990-01-03T22:53');
+      mount(<DateEditor field={fieldSdk} isInitiallyDisabled={false} parameters={parameters} />);
 
       selectors.getDateInput().should('have.value', '03 Jan 1990');
       selectors.getTimeInput().should('not.exist');
@@ -364,41 +347,33 @@ describe('Date Editor', () => {
     });
 
     it('correct actions are called when user interacts with editor', () => {
-      openPage();
+      const [fieldSdk] = createFakeFieldAPI();
+      mount(<DateEditor field={fieldSdk} isInitiallyDisabled={false} parameters={parameters} />);
+
+      cy.spy(fieldSdk, 'setValue').as('setValue');
+      cy.spy(fieldSdk, 'removeValue').as('removeValue');
 
       selectors.getCalendarTrigger().click();
       selectors.getCalendarTodayDate().click();
       selectors.getDateInput().should('have.value', getTodayShortString());
+      cy.get('@setValue').should('have.been.calledWith', getTodayKebabString());
 
       selectors.getClearBtn().click();
+      cy.get('@removeValue').should('have.been.called');
 
       selectors.getDateInput().should('have.value', '');
       selectors.getClearBtn().should('not.exist');
-
-      cy.editorEvents().should('deep.equal', [
-        { id: 4, type: 'onValueChanged', value: undefined },
-        { id: 3, type: 'removeValue', value: undefined },
-        {
-          id: 2,
-          type: 'onValueChanged',
-          value: getTodayKebabString(),
-        },
-        {
-          id: 1,
-          type: 'setValue',
-          value: getTodayKebabString(),
-        },
-      ]);
     });
 
     it('should reset field state on external change', () => {
-      cy.setInitialValue('1990-01-03');
+      const [fieldSdk] = createFakeFieldAPI(undefined, '1990-01-03');
+      mount(<DateEditor field={fieldSdk} isInitiallyDisabled={false} parameters={parameters} />);
 
-      openPage();
-
-      selectors.getDateInput().should('have.value', '03 Jan 1990');
-
-      cy.setValueExternal('1992-01-03');
+      //TODO: same as before
+      selectors
+        .getDateInput()
+        .should('have.value', '03 Jan 1990')
+        .then(() => fieldSdk.setValue('1992-01-03'));
 
       selectors.getDateInput().should('have.value', '03 Jan 1992');
     });
