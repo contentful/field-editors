1c30ceb3003d0629202402e5b77fffaf3c9b1b31
Yuri Mazursky <colomolo@users.noreply.github.com>
2023-12-22 14:31:32 +0000

    test: add fixtures

---
+++ cypress/component/rich-text/document-mocks/paragraphWithoutFormattings.js
@@ -0,0 +1,83 @@
+export default {
+  nodeType: 'document',
+  content: [
+    {
+      content: [
+        {
+          data: {},
+          marks: [
+            {
+              type: 'bold',
+            },
+          ],
+          nodeType: 'text',
+          value: 'What is Lorem Ipsum?',
+        },
+      ],
+      data: {},
+      nodeType: 'paragraph',
+    },
+    {
+      content: [
+        {
+          data: {},
+          marks: [
+            {
+              type: 'bold',
+            },
+          ],
+          nodeType: 'text',
+          value: 'Lorem Ipsum',
+        },
+        {
+          data: {},
+          marks: [],
+          nodeType: 'text',
+          value:
+            " is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
+        },
+        {
+          data: {},
+          marks: [
+            {
+              type: 'italic',
+            },
+          ],
+          nodeType: 'text',
+          value:
+            'It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum',
+        },
+        {
+          data: {},
+          marks: [],
+          nodeType: 'text',
+          value: ' ',
+        },
+        {
+          data: {},
+          marks: [
+            {
+              type: 'bold',
+            },
+          ],
+          nodeType: 'text',
+          value:
+            'passages, and more recently with desktop publishing software like Aldus PageMaker including ',
+        },
+        {
+          data: {},
+          marks: [
+            {
+              type: 'underline',
+            },
+          ],
+          nodeType: 'text',
+          value: 'versions of Lorem Ipsum.',
+        },
+      ],
+      data: {},
+      nodeType: 'paragraph',
+    },
+  ],
+  data: {},
+};
