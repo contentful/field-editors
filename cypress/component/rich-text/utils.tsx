f9b246138b49d4a58af358cee328b617087f0f19
Yuri Mazursky <colomolo@users.noreply.github.com>
2024-02-20 12:05:57 +0000

    test: fix mount props passing

--- cypress/component/rich-text/utils.tsx
+++ cypress/component/rich-text/utils.tsx
@@ -4,20 +4,11 @@ import { RichTextEditor } from '../../../packages/rich-text/src';
 import { createRichTextFakeSdk } from '../../fixtures';
 import { mount } from '../mount';
 
-interface MountRichTextEditorOptions extends Partial<React.ComponentProps<typeof RichTextEditor>> {
-  sdfsdf?: boolean;
-}
+type MountRichTextEditorOptions = Partial<React.ComponentProps<typeof RichTextEditor>>;
 
 export const mountRichTextEditor = (options: MountRichTextEditorOptions = {}): void => {
-  const { sdk, actionsDisabled = false, isInitiallyDisabled = false, onAction } = options;
+  const { sdk, isInitiallyDisabled = false } = options;
   const rtSDK = sdk || createRichTextFakeSdk();
 
-  mount(
-    <RichTextEditor
-      sdk={rtSDK}
-      isInitiallyDisabled={isInitiallyDisabled}
-      actionsDisabled={actionsDisabled}
-      onAction={onAction}
-    />
-  );
+  mount(<RichTextEditor sdk={rtSDK} isInitiallyDisabled={isInitiallyDisabled} {...options} />);
 };
