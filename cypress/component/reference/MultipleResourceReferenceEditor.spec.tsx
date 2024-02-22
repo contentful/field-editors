9b0b3584f32ac7e62e0e6510166c6843f4873524
Rebecca König <veu@users.noreply.github.com>
2024-02-22 17:17:43 +0100

    chore: use new cross-space entity picker functions (#1614)

--- cypress/component/reference/MultipleResourceReferenceEditor.spec.tsx
+++ cypress/component/reference/MultipleResourceReferenceEditor.spec.tsx
@@ -31,7 +31,7 @@ const commonProps = {
 };
 
 //TODO: Tests for custom cards after implementation of handling,
-//fix 'dialogs.selectSingleResourceEntry' error in linkExisting click handling
+//fix 'dialogs.selectSingleResourceEntity' error in linkExisting click handling
 
 describe('Multiple resource editor', () => {
   const findLinkExistingBtn = () => cy.findByTestId('linkEditor.linkExisting');
