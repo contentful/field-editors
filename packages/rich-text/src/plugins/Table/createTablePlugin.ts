9149e1d4c5bea79dd3df395b9657a30b4f323ee2
Aodhagan Murphy <aodhagan.murphy@contentful.com>
2024-09-30 14:51:56 +0100

    fix: adding ctx to table plugin function [TOL-2415]

--- packages/rich-text/src/plugins/Table/createTablePlugin.ts
+++ packages/rich-text/src/plugins/Table/createTablePlugin.ts
@@ -45,8 +45,7 @@ export const createTablePlugin = (): PlatePlugin =>
       const { normalizeNode } = editor;
       // injects important fixes from plate's original table plugin
       editor = withDeleteTable(editor);
-      // @ts-expect-error -- .
-      editor = withGetFragmentTable(editor);
+      editor = withGetFragmentTable(editor, plugin);
       editor = withInsertFragmentTable(editor, plugin);
       // overrides insertFragment to handle table insertion to not add empty paragraph before table
       editor = withInsertFragmentTableOverride(editor);
