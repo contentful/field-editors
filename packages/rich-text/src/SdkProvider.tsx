80f9341fd741409df1775ebd1c67317bc203ba62
Yves Rijckaert <hi@yvesrijckaert.com>
2025-06-06 12:28:15 +0200

    fix: rerender sdk when release params change

--- packages/rich-text/src/SdkProvider.tsx
+++ packages/rich-text/src/SdkProvider.tsx
@@ -8,7 +8,7 @@ interface SdkProviderProps {
 }
 
 function useSdk({ sdk }: SdkProviderProps) {
-  const sdkMemo = React.useMemo<FieldAppSDK>(() => sdk, []); // eslint-disable-line -- TODO: explain this disable
+  const sdkMemo = React.useMemo<FieldAppSDK>(() => sdk, [sdk.parameters.instance.release]); // eslint-disable-line -- TODO: explain this disable
 
   return sdkMemo;
 }
