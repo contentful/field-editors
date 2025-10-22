2bcadbf1c209e534718c4f348f1901a4f478bded
z0al <12673605+z0al@users.noreply.github.com>
2025-10-22 13:15:11 +0100

    fix(rich-text): compute count for initial value (#1959)

--- packages/rich-text/src/plugins/CharCounter/withCharCounter.ts
+++ packages/rich-text/src/plugins/CharCounter/withCharCounter.ts
@@ -1,16 +1,26 @@
+import debounce from 'p-debounce';
+
 import { PlateEditor } from '../../internal';
 import { getTextContent } from './utils';
-import debounce from 'p-debounce';
+
+const CHARACTER_COUNT_DEBOUNCE_TIME = 300;
 
 export const withCharCounter = (editor: PlateEditor) => {
   const { apply } = editor;
 
-  let count = getTextContent(editor).length;
-  editor.getCharacterCount = () => count;
+  let count: number | undefined;
+
+  editor.getCharacterCount = () => {
+    if (count === undefined) {
+      count = getTextContent(editor).length;
+    }
+
+    return count;
+  };
 
   const recount = debounce(async () => {
     count = getTextContent(editor).length;
-  }, 300);
+  }, CHARACTER_COUNT_DEBOUNCE_TIME);
 
   editor.apply = (op) => {
     apply(op);
