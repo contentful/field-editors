e489de9bd5910ec5c8d5f23013848d5f0603c36f
Josélia Costa <9259883+joseliacosta@users.noreply.github.com>
2026-01-13 10:58:36 +0100

    docs: specify and type field wrapper sdk story mock data [TOL-2494](#2041)

--- packages/default-field-editors/stories/DefaultFieldEditors.stories.tsx
+++ packages/default-field-editors/stories/DefaultFieldEditors.stories.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import { Text } from '@contentful/f36-components';
 import { PencilSimpleIcon, InfoIcon } from '@contentful/f36-icons';
 import { BooleanEditor } from '@contentful/field-editor-boolean';
+import { FieldAppSDK } from '@contentful/field-editor-shared';
 import {
   ActionsPlayground,
   createFakeFieldAPI,
@@ -28,40 +29,112 @@ export const Default: Story = {
     controls: { hideNoControlsWarning: true },
   },
   render: () => {
-    const [field, mitt] = createFakeFieldAPI(
-      // @ts-expect-error
+    // Create separate field APIs for different field types
+    const [textField, mitt] = createFakeFieldAPI(
       (mock) => ({
         ...mock,
-        items: {
-          validations: [{ in: ['test1', 'test2'] }],
-        },
+        id: 'text-field-id',
+        name: 'field',
+        type: 'Symbol', // Text field type
       }),
-      [],
+      '',
     );
+
+    const [ratingField] = createFakeFieldAPI(
+      (mock) => ({
+        ...mock,
+        id: 'rating-field-id',
+        name: 'anotherField',
+        type: 'Integer', // Rating is a number
+      }),
+      undefined,
+    );
+
+    const [dateField] = createFakeFieldAPI(
+      (mock) => ({
+        ...mock,
+        id: 'date-field-id',
+        name: 'customField',
+        type: 'Date', // Date picker field type
+      }),
+      undefined,
+    );
+
+    const [booleanField] = createFakeFieldAPI(
+      (mock) => ({
+        ...mock,
+        id: 'boolean-field-id',
+        name: 'customEditor',
+        type: 'Boolean',
+      }),
+      false,
+    );
+
     const space = createFakeSpaceAPI();
     const locales = createFakeLocalesAPI();
-    const sdk = {
-      field,
+
+    // Create SDK for text field
+    const textSdk: FieldAppSDK = {
+      field: textField,
       space,
       locales,
       contentType: { displayField: 'name' },
       parameters: {
         instance: {
-          helpText: 'Field help text',
+          helpText: 'This is a text field',
         },
       },
-    };
+    } as unknown as FieldAppSDK;
+
+    // Create SDK for rating field
+    const ratingSdk: FieldAppSDK = {
+      field: ratingField,
+      space,
+      locales,
+      contentType: { displayField: 'name' },
+      parameters: {
+        instance: {
+          helpText: 'This is a rating field',
+        },
+      },
+    } as unknown as FieldAppSDK;
+
+    // Create SDK for date field
+    const dateSdk: FieldAppSDK = {
+      field: dateField,
+      space,
+      locales,
+      contentType: { displayField: 'name' },
+      parameters: {
+        instance: {
+          helpText: 'This is a date field',
+        },
+      },
+    } as unknown as FieldAppSDK;
+
+    // Create SDK for boolean field
+    const booleanSdk: FieldAppSDK = {
+      field: booleanField,
+      space,
+      locales,
+      contentType: { displayField: 'name' },
+      parameters: {
+        instance: {
+          helpText: 'This is a boolean field',
+        },
+      },
+    } as unknown as FieldAppSDK;
 
     return (
       <div data-test-id="default-field-editors-test">
-        <FieldWrapper sdk={sdk as any} name="field">
-          <Field sdk={sdk as any} widgetId="singleLine" />
+        <FieldWrapper sdk={textSdk} name="field">
+          <Field sdk={textSdk} widgetId="singleLine" />
         </FieldWrapper>
-        <FieldWrapper sdk={sdk as any} name="anotherField">
-          <Field sdk={sdk as any} widgetId="rating" />
+        <FieldWrapper sdk={ratingSdk} name="anotherField">
+          <Field sdk={ratingSdk} widgetId="rating" />
         </FieldWrapper>
         <FieldWrapper
-          sdk={sdk as any}
+          sdk={dateSdk}
           name="customField"
           renderHeading={(name) => (
             <div>
@@ -74,11 +147,11 @@ export const Default: Story = {
             </Text>
           )}
         >
-          <Field sdk={sdk as any} widgetId="datePicker" />
+          <Field sdk={dateSdk} widgetId="datePicker" />
         </FieldWrapper>
-        <FieldWrapper sdk={sdk as any} name="customEditor">
+        <FieldWrapper sdk={booleanSdk} name="customEditor">
           <Field
-            sdk={sdk as any}
+            sdk={booleanSdk}
             renderFieldEditor={(widgetId, sdk, isInitiallyDisabled) => {
               return (
                 <div>
