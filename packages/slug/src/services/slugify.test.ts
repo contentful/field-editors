c38ed5f7e8d9a2bdf7bbe95ad7d9baa710bb4ad7
z0al <12673605+z0al@users.noreply.github.com>
2026-04-23 15:44:26 +0100

    test: update unit tests

--- packages/slug/src/services/slugify.test.ts
+++ packages/slug/src/services/slugify.test.ts
@@ -2,7 +2,7 @@ import { slugify } from './slugify';
 
 describe('slugify', () => {
   const cases = [
-    ['We ♥ $ & €', 'we-love-usd-and-eur'],
+    ['We ♥ $ & €', 'we-love-and-eur'],
     ['it`s a Slug', 'its-a-slug'],
     ['it’S a slug', 'its-a-slug'],
     ["it's a SLUG", 'its-a-slug'],
