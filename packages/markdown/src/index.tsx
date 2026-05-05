2e5fb463556f91618789b2e0b20a6b28469709e4
Chris Helgert <chrishelgert@users.noreply.github.com>
2026-05-05 16:39:06 +0200

    fix: add missing type export for MarkdownTab [] (#2153)

--- packages/markdown/src/index.tsx
+++ packages/markdown/src/index.tsx
@@ -4,3 +4,4 @@ export { MarkdownEditorConnected as MarkdownEditor } from './MarkdownEditor';
 export { default as MarkdownPreview } from './components/MarkdownPreview';
 export { openMarkdownDialog } from './dialogs/openMarkdownDialog';
 export { renderMarkdownDialog } from './dialogs/renderMarkdownDialog';
+export type { MarkdownTab } from './types';
