4517f40309544562eeaa276d6cd74106f262dc7d
Maya Gillilan <maya.gillilan@contentful.com>
2026-07-01 14:54:27 +0200

    test: activate i18n in vitest

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,8 +1,10 @@
+import { i18n } from '@lingui/core';
 import { cleanup } from '@testing-library/react';
 import { afterEach, expect } from 'vitest';
-
 import '@testing-library/jest-dom/vitest';
 import * as matchers from 'vitest-axe/matchers';
+
+i18n.activate('en-US');
 
 expect.extend(matchers);
 
