5217726c9f20a33a81ae32a205f7212b4aff335b
Maya Gillilan <maya.gillilan@contentful.com>
2026-07-01 12:56:05 +0200

    fix: show max char count capped by technical limits, add missing translations

--- packages/_shared/src/CharValidation.tsx
+++ packages/_shared/src/CharValidation.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 
+import { Trans } from '@lingui/react';
+
 import { ValidationType } from './types';
 
 interface CharValidationProps {
@@ -10,13 +12,33 @@ export function CharValidation(props: CharValidationProps) {
   const { constraints } = props;
 
   if (constraints.type === 'max') {
-    return <span>Maximum {constraints.max} characters</span>;
+    return (
+      <span>
+        <Trans
+          id="FieldEditors.Shared.CharValidation.MaxConstraint"
+          values={{ max: constraints.max }}
+          message="Maximum {max} characters"
+        />
+      </span>
+    );
   } else if (constraints.type === 'min') {
-    return <span>Requires at least {constraints.min} characters</span>;
+    return (
+      <span>
+        <Trans
+          id="FieldEditors.Shared.CharValidation.MinConstraint"
+          values={{ min: constraints.min }}
+          message="Requires at least {min} characters"
+        />
+      </span>
+    );
   } else {
     return (
       <span>
-        Requires between {constraints.min} and {constraints.max} characters
+        <Trans
+          id="FieldEditors.Shared.CharValidation.MinMaxConstraint"
+          values={{ max: constraints.max, min: constraints.min }}
+          message="Requires between {min} and {max} characters"
+        />
       </span>
     );
   }
